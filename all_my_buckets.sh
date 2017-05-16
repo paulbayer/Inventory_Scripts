30b9804d1efe7145fd9af93ee7cd4454cc3ec338
Paul Bayer <paulbaye@amazon.com>
2017-05-16 14:39:38 -0400

    Added new script to capture all S3 buckets in all profiles

---
+++ all_my_buckets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a AllProfiles
+
+#AllProfiles=(default Primary Secondary Nasdaq-Prod Nasdaq-Dev Nasdaq-DR)
+AllProfiles=( $(egrep '\[.*\]' ~/.aws/credentials | tr -d '[]\r') )
+
+echo "Outputting all S3 buckets from all profiles"
+
+printf "%-20s %-50s \n" "Profile" "Bucket Name" 
+printf "%-20s %-50s \n" "-------" "-----------"
+for profile in ${AllProfiles[@]}; do
+		aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile | awk -F $"\t" -v var=${profile} '{for (i=1;i<=NF;i++) printf "%-20s %-50s \n",var,$i}' 
+done
+
+echo
+exit 0
