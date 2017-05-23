228ccdcaccf5115e2bfc0374e7d3bfa448eb2da1
Paul Bayer <paulbaye@amazon.com>
2017-05-23 17:35:35 -0400

    Updated S3 script to accomodate number of files and total size of used space.

--- all_my_buckets.sh
+++ all_my_buckets.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 declare -a AllProfiles
+declare -a AllBuckets
 
 #AllProfiles=(default Primary Secondary Nasdaq-Prod Nasdaq-Dev Nasdaq-DR)
 AllProfiles=( $(egrep '\[.*\]' ~/.aws/credentials | tr -d '[]\r') )
+
+# AllBuckets=(
 
 echo "Outputting all S3 buckets from all profiles"
 
 printf "%-20s %-50s \n" "Profile" "Bucket Name" 
 printf "%-20s %-50s \n" "-------" "-----------"
 for profile in ${AllProfiles[@]}; do
-		aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile | awk -F $"\t" -v var=${profile} '{for (i=1;i<=NF;i++) printf "%-20s %-50s \n",var,$i}' 
+	aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile | awk -F $"\t" -v var=${profile} '{for (i=1;i<=NF;i++) printf "%-20s %-50s \n",var,$i}' 
+
+	# aws s3api list-objects --bucket $bucketname --query 'Contents[].[Key,Size]' --output text -- profile $profile | awk '{s+=$2;++i}; END {printf "%s %d %s %d %s \n","There are:",i,"files in the bucket; totaling:",s,"bytes."}'
 done
 
 echo
