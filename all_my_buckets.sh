90cd02eb1ff6e61323f664d5dbdf24499821b99e
Paul Bayer <paulbaye@amazon.com>
2017-05-24 13:52:04 -0400

    Updated

--- all_my_buckets.sh
+++ all_my_buckets.sh
@@ -16,7 +16,7 @@ for profile in ${AllProfiles[@]}; do
 	ProfileBuckets=( $(aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile) ) 
 	# aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile | awk -F $"\t" -v var=${profile} '{for (i=1;i<=NF;i++) printf "%-20s %-50s \n",var,$i}' 
 	for bucket in ${ProfileBuckets[@]}; do
-		aws s3api list-objects --bucket $bucket --query 'Contents[].[Key,Size]' --output text --profile $profile | awk -v bucketname=${bucket} -v profilename=${profile} '{s+=$2;++i}; END {printf "%-20s %-60s %'"'"'15d %'"'"'18d \n",profilename,bucketname,i,s; }' 
+			aws s3api list-objects --bucket $bucket --query 'Contents[].[Key,Size]' --output text --profile $profile | awk -v bucketname=${bucket} -v profilename=${profile} '{s+=$2;++i;}; END {printf "%-20s %-60s %'"'"'15d %'"'"'18d \n",profilename,bucketname,i,s; }' 
 	done
 	echo "-----------------------"
 done
