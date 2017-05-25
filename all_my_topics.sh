d370f7359a06c1e6a8a98c8f4f9e28a9767eb57e
Paul Bayer <paulbaye@amazon.com>
2017-05-25 10:02:07 -0400

    Added an SNS topic finder

---
+++ all_my_topics.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -a AllProfiles
+declare -a ProfileBuckets
+
+#AllProfiles=(default Primary Secondary Nasdaq-Prod Nasdaq-Dev Nasdaq-DR)
+AllProfiles=( $(egrep '\[.*\]' ~/.aws/credentials | tr -d '[]\r') )
+
+NumofProfiles=${#AllProfiles[@]}
+echo "Found ${NumofProfiles} profiles in credentials file"
+echo "Outputting all SNS topics from all profiles"
+
+printf "%-20s %-50s \n" "Profile" "Topic Name"
+printf "%-20s %-50s \n" "-------" "-----------"
+for profile in ${AllProfiles[@]}; do
+	aws sns list-topics --output text --query 'Topics[].TopicArn' --profile $profile | awk -F $"\t" -v var=${profile} '{for (i=1;i<=NF;i++) printf "%-20s %-50s \n",var,$i}' 
+	echo "----------------"
+done
+
+echo
+exit 0
