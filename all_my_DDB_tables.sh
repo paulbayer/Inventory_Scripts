1e24106216ce09dca7eb5a0f24950ca8b39dd42c
Paul Bayer <paulbaye@amazon.com>
2017-06-29 14:31:44 -0400

    Updated menu and added new script for DDB Tables

---
+++ all_my_DDB_tables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a AllProfiles
+
+AllProfiles=( $(egrep '\[.*\]' ~/.aws/credentials | tr -d '[]\r') )
+
+NumofProfiles=${#AllProfiles[@]}
+echo "Found ${NumofProfiles} profiles in credentials file"
+echo "Outputting all DynamoDB Tables from all profiles"
+
+printf "%-20s %-50s \n" "Profile" "Table Name"
+printf "%-20s %-50s \n" "-------" "-----------"
+for profile in ${AllProfiles[@]}; do
+	aws dynamodb list-tables --output text --query 'TableNames' --profile $profile | tr "\t" "\n" | awk -F $"\t" -v var=${profile} '{printf "%-20s %-50s \n",var,$1}' 
+	echo "----------------"
+done
+
+echo
+exit 0
