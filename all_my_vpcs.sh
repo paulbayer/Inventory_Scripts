6e904ae909fed37567a575cf399e1d3d0af65047
Paul Bayer <paulbaye@amazon.com>
2017-09-07 11:18:37 -0400

    Added all_my_vpcs

--- all_my_vpcs.sh
+++ all_my_vpcs.sh
@@ -9,10 +9,10 @@ NumofProfiles=${#AllProfiles[@]}
 echo "Found ${NumofProfiles} profiles in credentials file"
 echo "Outputting all VPCs from all profiles"
 
-printf "%-20s %-12s %-12s %-15s \n" "Profile" "VPC ID" "State" "Cidr Block"
-printf "%-20s %-12s %-12s %-15s \n" "-------" "------" "-----" "----------"
+printf "%-20s %-15s %-15s %-15s \n" "Profile" "VPC ID" "State" "Cidr Block"
+printf "%-20s %-15s %-15s %-15s \n" "-------" "------" "-----" "----------"
 for profile in ${AllProfiles[@]}; do
-	aws ec2 describe-vpcs --query 'Vpcs[].[VpcId,State,CidrBlock]' --output text --profile $profile | awk -F $"\t" -v var=${profile} '{printf "%-20s %-12s %-12s %-15s \n",var,$1,$2,$3}'
+	aws ec2 describe-vpcs --query 'Vpcs[].[VpcId,State,CidrBlock]' --output text --profile $profile | awk -F $"\t" -v var=${profile} '{printf "%-20s %-15s %-15s %-15s \n",var,$1,$2,$3}'
 	echo "----------------"
 done
 
