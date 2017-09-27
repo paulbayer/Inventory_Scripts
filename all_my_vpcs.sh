f9420512521086dc76020396591d1dace4acc72e
Paul Bayer <paulbaye@amazon.com>
2017-09-27 01:11:39 -0400

    Updated vpcs to show profiles via role assumption

--- all_my_vpcs.sh
+++ all_my_vpcs.sh
@@ -3,16 +3,18 @@
 declare -a AllProfiles
 
 #AllProfiles=(default Primary Secondary Nasdaq-Prod Nasdaq-Dev Nasdaq-DR)
-AllProfiles=( $(egrep '\[.*\]' ~/.aws/credentials | tr -d '[]\r') )
+#AllProfiles=( $(egrep '\[.*\]' ~/.aws/credentials | tr -d '[]\r') )
+AllProfiles=( $(~/GitRepos/Inventory_Scripts/Allprofiles.sh | awk 'NR>2 {print $1}') )
 
 NumofProfiles=${#AllProfiles[@]}
 echo "Found ${NumofProfiles} profiles in credentials file"
 echo "Outputting all VPCs from all profiles"
+format='%-20s %-15s %-15s %-15s \n'
 
-printf "%-20s %-15s %-15s %-15s \n" "Profile" "VPC ID" "State" "Cidr Block"
-printf "%-20s %-15s %-15s %-15s \n" "-------" "------" "-----" "----------"
+printf "$format" "Profile" "VPC ID" "State" "Cidr Block"
+printf "$format" "-------" "------" "-----" "----------"
 for profile in ${AllProfiles[@]}; do
-	aws ec2 describe-vpcs --query 'Vpcs[].[VpcId,State,CidrBlock]' --output text --profile $profile | awk -F $"\t" -v var=${profile} '{printf "%-20s %-15s %-15s %-15s \n",var,$1,$2,$3}'
+	aws ec2 describe-vpcs --query 'Vpcs[].[VpcId,State,CidrBlock]' --output text --profile $profile | awk -F $"\t" -v var=${profile} -v fmt="${format}" '{printf fmt,var,$1,$2,$3}'
 	echo "----------------"
 done
 
