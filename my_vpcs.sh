809dc53e31531542309bccda9f7bdfd679d6b3c0
Paul Bayer <paulbaye@amazon.com>
2017-09-27 21:52:18 -0400

    Updated profiles scripts

---
+++ my_vpcs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+profile=$1
+
+if [[ -z $profile ]]
+	then
+		echo
+		echo "This command requires that you pass in the AWS profile name you're looking up"
+		echo "Therefore, the script should be run like this:"
+		echo "	$0 <profile name>"
+		echo
+		exit 1
+fi
+
+echo "Outputting all VPCs, only from $profile"
+format='%-20s %-15s %-15s %-15s \n'
+
+printf "$format" "Profile" "VPC ID" "State" "Cidr Block"
+printf "$format" "-------" "------" "-----" "----------"
+echo
+aws ec2 describe-vpcs --query 'Vpcs[].[VpcId,State,CidrBlock]' --output text --profile $profile | awk -F $"\t" -v var=${profile} -v fmt="${format}" '{printf fmt,var,$1,$2,$3}'
+
+echo
+exit 0
