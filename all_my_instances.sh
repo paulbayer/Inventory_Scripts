892832bf75776093be7513a57118104313fcc361
Paul Bayer <paulbaye@amazon.com>
2017-10-25 13:46:11 -0400

    Changed AllProfiles.sh a little bit

--- all_my_instances.sh
+++ all_my_instances.sh
@@ -2,15 +2,18 @@
 
 declare -a AllProfiles
 
+echo "Gathering profiles..."
 #AllProfiles=(default Primary Secondary Nasdaq-Prod Nasdaq-Dev Nasdaq-DR)
-AllProfiles=( $(~/GitRepos/Inventory_Scripts/Allprofiles.sh | awk '(NR>5 && $1 !~ /^-/) {print $1}') )
+AllProfiles=( $(~/GitRepos/Inventory_Scripts/Allprofiles.sh auto | awk '(NR>5 && $1 !~ /^-/) {print $1}') )
+
+format='%-20s %-25s %-50s %-10s %-15s \n'
 
 echo "Outputting all EC2 instances from all profiles"
 
-printf "%-20s %-25s %-50s %-10s %-15s \n" "Profile" "Instance Name" "Public DNS Name" "State" "Instance ID"
-printf "%-20s %-25s %-50s %-10s %-15s \n" "-------" "-------------" "---------------" "-----" "-----------"
+printf "$format" "Profile" "Instance Name" "Public DNS Name" "State" "Instance ID"
+printf "$format" "-------" "-------------" "---------------" "-----" "-----------"
 for profile in ${AllProfiles[@]}; do
-	aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`]|[0].Value,PublicDnsName,State.Name,InstanceId]' --profile $profile | awk -F $"\t" -v var=${profile} '{printf "%-20s %-25s %-50s %-10s %-15s \n",var,$1,$2,$3,$4}' 
+	aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`]|[0].Value,PublicDnsName,State.Name,InstanceId]' --profile $profile | awk -F $"\t" -v var=${profile} -v fmt="${format}" '{printf fmt,var,$1,$2,$3,$4}'
 done
 
 echo
