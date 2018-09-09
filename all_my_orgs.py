ac2a5b6e6236789c387621ca544b535eb4eb4d81
Paul Bayer <paulbaye@amazon.com>
2018-09-08 20:20:04 -0400

    The Org script works

--- all_my_orgs.py
+++ all_my_orgs.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
-import os, sys, argparse
+#import os, sys
+import argparse
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
 from colorama import init,Fore,Back,Style
@@ -8,7 +9,7 @@ from colorama import init,Fore,Back,Style
 init()
 
 UsageMsg="You can provide a level to determine whether this script considers only the 'credentials' file, the 'config' file, or both."
-parser = argparse.ArgumentParser(description="We\'re going to find all publicly open S3 buckets.",prefix_chars='-+/')
+parser = argparse.ArgumentParser(description="We\'re going to find all accounts within any of the organizations we have access to.",prefix_chars='-+/')
 parser.add_argument("-c","--creds", dest="plevel", metavar="Creds", default="1", help="Which credentials file to use for investigation.")
 
 args=parser.parse_args()
@@ -19,8 +20,6 @@ args=parser.parse_args()
 	# 3: credentials and config files
 plevel=args.plevel
 
-fRegion = "us-east-1"
-
 def find_org_root(fProfile):
 
 	session_org = boto3.Session(profile_name=fProfile)
@@ -28,6 +27,14 @@ def find_org_root(fProfile):
 	response=client_org.describe_organization()
 	root_org=response['Organization']['MasterAccountId']
 	return (root_org)
+
+def find_acct_email(fProfile,fAccount):
+
+	session_org = boto3.Session(profile_name=fProfile)
+	client_org = session_org.client('organizations')
+	response=client_org.describe_account(AccountId=fAccount)
+	email_addr=response['Account']['Email']
+	return (email_addr)
 
 def find_child_accounts(fProfile):
 
@@ -47,8 +54,7 @@ def find_account_number(fProfile):
 	return (acct_num)
 
 def get_profiles(flevel):
-
-# If flevel
+	# If flevel
 	# 1: credentials file only
 	# 2: config file only
 	# 3: credentials and config files
@@ -57,7 +63,7 @@ def get_profiles(flevel):
 
 	profiles = []
 
-	if flevel == 3:	# Credentials and Config file
+	if flevel == "3":	# Credentials and Config file
 		with open(home+"/.aws/credentials","r") as creds_file:
 			cred_data = creds_file.readlines()
 
@@ -76,7 +82,7 @@ def get_profiles(flevel):
 				# print (profile)
 				profiles.append(profile)
 
-	elif flevel == 2: # Config file only
+	elif flevel == "2": # Config file only
 		with open(home+"/.aws/config","r") as config_file:
 			conf_data = config_file.readlines()
 
@@ -98,26 +104,28 @@ def get_profiles(flevel):
 
 	return(profiles)
 
-fmt='%-20s %-20s %-40s %-10s'
+fmt='%-20s %-20s %-30s %-35s %-10s'
 print ("------------------------------------")
-print (fmt % ("Profile Name","Account Number","Master Org Acct","Root Acct?"))
-print (fmt % ("------------","--------------","---------------","----------"))
+print (fmt % ("Profile Name","Account Number","Master Org Acct","Email","Root Acct?"))
+print (fmt % ("------------","--------------","---------------","-----","----------"))
 
-RootAccts=[]
-RootProfiles=[]
+RootAccts=[]	# List of the Organization Root's Account Number
+RootProfiles=[]	# List of the Organization Root's profiles
 for profile in get_profiles(plevel):
 	AcctNum = "Blank Acct"
 	MasterAcct = "Blank Root"
+	Email = "Email not available"
 	ErrorFlag = False
 	try:
 		AcctNum = find_account_number(profile)
 		MasterAcct = find_org_root(profile)
+		Email = find_acct_email(profile,AcctNum)
 	except ClientError as my_Error:
 		ErrorFlag = True
 		if str(my_Error).find("AWSOrganizationsNotInUseException") > 0:
 			MasterAcct="Not an Org Account"
 		elif str(my_Error).find("AccessDenied") > 0:
-			MasterAcct="Not authorized for API call."
+			MasterAcct="Acct not auth for Org API."
 		else:
 			print(my_Error)
 	except NoCredentialsError as my_Error:
@@ -134,20 +142,23 @@ for profile in get_profiles(plevel):
 	else:
 		RootAcct=False
 
+# Print results for this profile
 	if RootAcct:
-		print (Fore.RED + fmt % (profile,AcctNum,MasterAcct,RootAcct)+Style.RESET_ALL)
+		print (Fore.RED + fmt % (profile,AcctNum,MasterAcct,Email,RootAcct)+Style.RESET_ALL)
 	else:
-		print (fmt % (profile,AcctNum,MasterAcct,RootAcct))
+		print (fmt % (profile,AcctNum,MasterAcct,Email,RootAcct))
 
 print ("-------------------")
 
 # print("Root Accounts found:",set(RootAccts))
 # print("Root Profiles found:",set(RootProfiles))
 
-fmt='%-25s %-40s'
-print(fmt % ("Organization's Profile","Set of Organization Accounts"))
-print(fmt % ("----------------------","----------------------------"))
+fmt='%-25s'+ Style.BRIGHT +'%-15s'+ Style.RESET_ALL +'%-40s'
+print()
+print(fmt % ("Organization's Profile","Root Account","Set of Organization Accounts"))
+print(fmt % ("----------------------","------------","----------------------------"))
 for profile in RootProfiles:
 	child_accounts=[]
+	MasterAcct=find_org_root(profile)
 	child_accounts=find_child_accounts(profile)
-	print(fmt % (profile,child_accounts))
+	print(fmt % (profile,MasterAcct,child_accounts))
