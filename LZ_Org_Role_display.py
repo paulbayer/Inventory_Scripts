019136d8f645da17f7fe7f8f01a029833bf6f2a5
Paul Bayer <paulbaye@amazon.com>
2019-01-29 20:50:17 -0500

    Updating LZ_Org_Role_Display.py

--- LZ_Org_Role_display.py
+++ LZ_Org_Role_display.py
@@ -19,16 +19,6 @@ parser.add_argument(
 	default="all",
 	help="To specify a specific profile, use this parameter. There is no default for this")
 parser.add_argument(
-	"-b","--awsservice",
-	dest="pService",
-	metavar="Service",
-	help="Service that we're running boto3 for (e.g. 'cloudformation', 'guardduty', etc.)")
-parser.add_argument(
-	"-c","--command",
-	dest="pCommand",
-	metavar="Command to run",
-	help="Command to be run in multiple accounts and across multiple regions")
-parser.add_argument(
 	"-r","--region",
 	nargs="*",
 	dest="pRegion",
@@ -71,39 +61,29 @@ RegionList=Inventory_Modules.get_ec2_regions(pRegionList)
 ChildAccounts=Inventory_Modules.find_child_accounts2(pProfile)
 all_responses=[]
 
-# try:
-# 	session_aws=boto3.Session(profile_name=pProfile,region_name="us-east-1")
-# 	client_aws=session_aws.client('iam')
-# 	command_to_run='client_aws.'+pCommand
-# 	logging.warning("Command to be run: %s" % (command_to_run))
-# 	response=eval(command_to_run)
-# except ClientError as my_Error:
-# 	if str(my_Error).find("AuthFailure") > 0:
-# 		print(profile+": Authorization Failure")
-# all_responses.append(response['Roles'])
-
+command_to_run='client_aws.list_roles()'
+region='us-east-1'
 
 for account in ChildAccounts:
 	# for region in RegionList:
 	try:
 		sts_session = boto3.Session(profile_name=pProfile)
-		sts_client = sts_session.client('sts',region_name='us-east-1')
+		sts_client = sts_session.client('sts',region_name=region)
 		role_arn = "arn:aws:iam::{}:role/AWSCloudFormationStackSetExecutionRole".format(account['AccountId'])
 		account_credentials = sts_client.assume_role(
 			RoleArn=role_arn,
 			RoleSessionName="IAMTestScript")['Credentials']
 		session_aws=boto3.Session(
 			aws_access_key_id=account_credentials['AccessKeyId'],	aws_secret_access_key=account_credentials['SecretAccessKey'], aws_session_token=account_credentials['SessionToken'],
-			region_name="us-east-1")
+			region_name=region)
 		client_aws=session_aws.client('iam')
-		command_to_run='client_aws.'+pCommand
 		logging.warning("Command to be run: %s on account: %s" % (command_to_run,account['AccountId']))
 		response=eval(command_to_run)
 		all_responses.append(response['Roles'])
 		theresmore=(response['IsTruncated']==True)
 		logging.warning("There's more?: %s" % (theresmore))
 		while theresmore:
-			command_to_run='client_aws.'+pCommand[:-1]+'Marker='+response['Marker']+')'
+			command_to_run='client_aws.list_roles('+'Marker='+response['Marker']+')'
 			response=eval(command_to_run)
 			all_responses.append(response['Roles'])
 			theresmore=(response['IsTruncated']==True)
