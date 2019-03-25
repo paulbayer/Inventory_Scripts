b2ac181e5401e2cbac22cdca455baf766b1df2a1
Surendar <surendar.m@gmail.com>
2019-03-25 16:31:18 +0530

    Added MFA status indicator

--- all_my_users_with_policies.sh
+++ all_my_users_with_policies.sh
@@ -10,12 +10,12 @@ echo "Found ${ProfileCount} profiles in credentials file"
 echo "Outputting all Users and their assigned policies (both attached and in-line) from all profiles"
 
 ## Screen Colors
-## nhl="[0m"
-## hl="[31m"
+## nhl=""
+## hl=""
 ## i=1
 
-printf "%-15s %-40s %-60s \n" "Profile" "User Name" "AttachedPolicies"
-printf "%-15s %-40s %-60s \n" "-------" "---------" "----------------"
+printf "%-15s %-40s %-5s %-60s \n" "Profile" "User Name" "MFA" "AttachedPolicies"
+printf "%-15s %-40s %-5s %-60s \n" "-------" "---------" "---" "----------------"
 # Cycles through each profile
 for profile in ${AllProfiles[@]}; do
 #	echo "Running for profile: $profile"
@@ -29,8 +29,12 @@ for profile in ${AllProfiles[@]}; do
 		## 	echo -n ${nhl}
 		## fi
 		# This will output each policy associated with the specific user
-		aws iam list-attached-user-policies --user-name $user --profile $profile --output text --query 'AttachedPolicies[].PolicyName' | tr '\t' '\n' | awk -F $"\t" -v var=${profile} -v var2=${user} '{printf "%-15s %-40s %-60s \n",var,var2,$1}'
-		aws iam list-user-policies --user-name $user --profile $profile --output text --query 'PolicyNames' | tr '\t' '\n' | awk -F $"\t" -v var=${profile} -v var2=${user} '{printf "%-15s %-40s %-60s \n",var,var2,$1}'
+#Checking MFA status
+		my_var=`aws iam list-mfa-devices --user-name $user --output text`
+        if [ -z "$my_var" ]; then MFA="No"; else MFA="Yes"; fi
+        #echo $MFA
+		aws iam list-attached-user-policies --user-name $user --profile $profile --output text --query 'AttachedPolicies[].PolicyName' | tr '\t' '\n' | awk -F $"\t" -v var=${profile} -v var2=${user} -v var3=${MFA} '{printf "%-15s %-40s %-5s %-60s \n",var,var2,var3,$1}'
+		aws iam list-user-policies --user-name $user --profile $profile --output text --query 'PolicyNames' | tr '\t' '\n' | awk -F $"\t" -v var=${profile} -v var2=${user} -v var3=${MFA} '{printf "%-15s %-40s %-5s %-60s \n",var,var2,var3,$1}'
 		## ((i++))
 	done
 	## i=1
@@ -40,3 +44,5 @@ done
 
 echo
 exit 0
+
+aws iam list-attached-user-policies --user-name welkin --profile default --output text --query 'AttachedPolicies[].PolicyName' | tr '\t' '\n' | awk -F $"\t" -v var=default -v var2=welkin '{printf "%-15s %-40s %-60s \n",var,var2,$1}'
