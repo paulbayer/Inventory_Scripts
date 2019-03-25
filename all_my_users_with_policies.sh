716dbe6000bbc62250ab195e4f3914297c62aac2
Surendar <msurendar.suren@gmail.com>
2019-03-25 19:54:32 +0530

    Removed unwanted lines and fixed alignment

--- all_my_users_with_policies.sh
+++ all_my_users_with_policies.sh
@@ -31,8 +31,8 @@ for profile in ${AllProfiles[@]}; do
 		# This will output each policy associated with the specific user
 #Checking MFA status
 		my_var=`aws iam list-mfa-devices --user-name $user --output text`
-        if [ -z "$my_var" ]; then MFA="No"; else MFA="Yes"; fi
-        #echo $MFA
+        	if [ -z "$my_var" ]; then MFA="No"; else MFA="Yes"; fi
+        	#echo $MFA
 		aws iam list-attached-user-policies --user-name $user --profile $profile --output text --query 'AttachedPolicies[].PolicyName' | tr '\t' '\n' | awk -F $"\t" -v var=${profile} -v var2=${user} -v var3=${MFA} '{printf "%-15s %-40s %-5s %-60s \n",var,var2,var3,$1}'
 		aws iam list-user-policies --user-name $user --profile $profile --output text --query 'PolicyNames' | tr '\t' '\n' | awk -F $"\t" -v var=${profile} -v var2=${user} -v var3=${MFA} '{printf "%-15s %-40s %-5s %-60s \n",var,var2,var3,$1}'
 		## ((i++))
@@ -44,5 +44,3 @@ done
 
 echo
 exit 0
-
-aws iam list-attached-user-policies --user-name welkin --profile default --output text --query 'AttachedPolicies[].PolicyName' | tr '\t' '\n' | awk -F $"\t" -v var=default -v var2=welkin '{printf "%-15s %-40s %-60s \n",var,var2,$1}'
