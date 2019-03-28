12114fa87cf20c91e2373d1f0672c6478cd54960
Paul Bayer <paulbaye@amazon.com>
2019-03-28 13:19:04 -0400

    Added new cfnstacks.py to look at accounts instead of profiles

--- all_my_cfnstacks-old.py
+++ all_my_cfnstacks-old.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-import os, sys, pprint, boto3
+import os, sys, pprint
 import Inventory_Modules
 import argparse
 from colorama import init,Fore,Back,Style
@@ -16,10 +16,11 @@ parser = argparse.ArgumentParser(
 	prefix_chars='-+/')
 parser.add_argument(
 	"-p","--profile",
-	dest="pProfile",
+	dest="pProfiles",
+	nargs="*",
 	metavar="profile to use",
-	default="default",
-	help="To specify a specific profile, use this parameter. Default will be your default profile.")
+	default=["all"],
+	help="To specify a specific profile, use this parameter. Default will be ALL profiles, including those in ~/.aws/credentials and ~/.aws/config")
 parser.add_argument(
 	"-f","--fragment",
 	dest="pstackfrag",
@@ -61,63 +62,66 @@ parser.add_argument(
 	const=logging.WARNING)
 args = parser.parse_args()
 
-pProfile=args.pProfile
+pProfiles=args.pProfiles
 pRegionList=args.pregion
 pstackfrag=args.pstackfrag
 pstatus=args.pstatus
 verbose=args.loglevel
 DeletionRun=args.DeletionRun
 logging.basicConfig(level=args.loglevel)
+# RegionList=[]
+
+# if pRegionList
+
+# SkipProfiles=["default"]
+SkipProfiles=["default","Shared-Fid"]
 
 ##########################
 ERASE_LINE = '\x1b[2K'
 
 NumStacksFound = 0
+NumRegions = 0
 print()
 fmt='%-20s %-15s %-15s %-50s'
-print(fmt % ("Account","Region","Stack Status","Stack Name"))
+print(fmt % ("Profile","Region","Stack Status","Stack Name"))
 print(fmt % ("-------","------","------------","----------"))
 RegionList=Inventory_Modules.get_ec2_regions(pRegionList)
-ChildAccounts=Inventory_Modules.find_child_accounts2(pProfile)
-# pprint.pprint(RegionList)
+ProfileList=Inventory_Modules.get_profiles(pProfiles,SkipProfiles)# pprint.pprint(RegionList)
 # sys.exit(1)
 StacksFound=[]
-sts_session = boto3.Session(profile_name=pProfile)
-sts_client = sts_session.client('sts')
-for account in ChildAccounts:
-	role_arn = "arn:aws:iam::{}:role/AWSCloudFormationStackSetExecutionRole".format(account['AccountId'])
-	logging.info("Role ARN: %s" % role_arn)
-	try:
-		account_credentials = sts_client.assume_role(
-			RoleArn=role_arn,
-			RoleSessionName="Find-Stacks")['Credentials']
-	except ClientError as my_Error:
-		if str(my_Error).find("AuthFailure") > 0:
-			print(profile+": Authorization Failure for account {}".format(account['AccountId']))
-	for region in RegionList:
+for pregion in RegionList:
+	NumRegions += 1
+	NumProfilesInvestigated = 0	# I only care about the last run - so I don't get profiles * regions.
+	for profile in ProfileList: #Inventory_Modules.get_profiles(pProfiles,plevel,SkipProfiles):
+		NumProfilesInvestigated += 1
 		try:
-			StackNum=0
-			Stacks=Inventory_Modules.find_stacks_in_acct(account_credentials,region,pstackfrag)
+			Stacks=Inventory_Modules.find_stacks(profile,pregion,pstackfrag,pstatus)
 			# pprint.pprint(Stacks)
 			StackNum=len(Stacks)
-			logging.warning("Account: %s | Region: %s | Found %s Stacks", account['AccountId'], region, StackNum )
-			print(ERASE_LINE,Fore.RED+"Account: {} Region: {} Found {} Stacks".format(account['AccountId'],region,StackNum)+Fore.RESET,end='\r')
+			logging.warning("Profile: %s | Region: %s | Found %s Stacks", profile, pregion, StackNum )
+			print(ERASE_LINE,Fore.RED+"Profile: {} Region: {} Found {} Stacks".format(profile,pregion,StackNum)+Fore.RESET,end='\r')
 		except ClientError as my_Error:
 			if str(my_Error).find("AuthFailure") > 0:
-				print(account['AccountId']+": Authorization Failure")
-		if StackNum > 0:
+				print(profile+": Authorization Failure")
+		if len(Stacks) > 0:
 			for y in range(len(Stacks)):
 				StackName=Stacks[y]['StackName']
 				StackStatus=Stacks[y]['StackStatus']
-				print(fmt % (account['AccountId'],region,StackStatus,StackName))
+				print(fmt % (profile,pregion,StackStatus,StackName))
 				NumStacksFound += 1
 				StacksFound.append({
 					'Profile':profile,
-					'Region':region,
+					'Region':pregion,
 					'StackName':StackName,
 					'StackStatus':Stacks[y]['StackStatus']})
+				"""
+				StacksFound[x][0]=profile
+				StacksFound[x][1]=region
+				StacksFound[x][2]=Stack Name
+				StacksFound[x][3]=Stack Status
+				"""
 print(ERASE_LINE)
-print(Fore.RED+"Found",NumStacksFound,"Stacks across",len(ChildAccounts),"accounts across",len(RegionList),"regions"+Fore.RESET)
+print(Fore.RED+"Found",NumStacksFound,"Stacks across",NumProfilesInvestigated,"profiles across",NumRegions,"regions"+Fore.RESET)
 print()
 # pprint.pprint(StacksFound)
 
@@ -138,4 +142,4 @@ elif DeletionRun:
 		response=Inventory_Modules.delete_stack(StacksFound[y]['Profile'],StacksFound[y]['Region'],StacksFound[y]['StackName'])
 
 
-print("Thanks for using this script...")
+print("Thanks for playing...")
