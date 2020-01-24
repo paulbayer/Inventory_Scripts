54e00d3208fed2eb60bf1451b124530b8be2a112
Paul Bayer <PaulBaye@amazon.com>
2020-01-24 01:31:32 +0000

    no message

--- Shell/all_my_buckets.sh
+++ Shell/all_my_buckets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+#set -x
 declare -a AllProfiles
-
+region="us-east-1"
 echo "Gathering your profiles..."
-AllProfiles=( $(AllProfiles.sh ProfileNameOnly | awk '{print $1}') )
+AllProfiles=( $(./AllProfiles.sh ProfileNameOnly | awk '{print $1}') )
 
 format='%-20s %-12s %-50s \n'
 
@@ -14,15 +14,15 @@ echo "Outputting all S3 buckets from all profiles"
 printf "$format" "Profile" "Region" "Bucket Name"
 printf "$format" "-------" "------" "-----------"
 for profile in ${AllProfiles[@]}; do
-	if [[ $1 ]]
-		then
-			region=$1
-		else
-			region=`aws ec2 describe-availability-zones --query 'AvailabilityZones[].RegionName' --output text --profile ${profile}|tr '\t' '\n' |sort -u`
-	fi
+#	if [[ $1 ]]
+#		then
+#			region="us-east-1"
+#		else
+#			region=`aws ec2 describe-availability-zones --query 'AvailabilityZones[].RegionName' --output text --profile ${profile}|tr '\t' '\n' |sort -u`
+#	fi
 	tput el
 	echo -ne "Checking Profile: $profile in region: $region\\r"
-	out=$(aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile --region $region| awk -F $"\t" -v var=${profile} -v rgn=${region} -v fmt="${format}" '{for (i=1;i<=NF;i++) printf fmt,var,rgn,$i}'|tee /dev/tty)
+	out=$(aws s3api list-buckets --output text --query 'Buckets[*].Name' --profile $profile --region $region | awk -F $"\t" -v var=${profile} -v rgn=${region} -v fmt="${format}" '{for (i=1;i<=NF;i++) printf fmt,var,rgn,$i}'|tee /dev/tty)
 	# echo "----- Output: "$out"-------"
 	if [[ $out ]]
 		then
--- delete_bucket_objects.py
+++ delete_bucket_objects.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
 #!/usr/local/bin/python3
 
 import boto3, sys
 
-bucket_name=sys.argv[1]
-pProfile=sys.argv[2]
 pProfile=sys.argv[1]
 forcequit=sys.argv[2]
 bucket_name=sys.argv[3]
@@ -12,7 +9,6 @@ session = boto3.Session(profile_name=pProfile)
 s3 = session.resource(service_name='s3')
 bucket = s3.Bucket(bucket_name)
 bucket.object_versions.delete()
-x = input("Now that the bucket is empty, do you want to delete the bucket? (y/n): ")
 if forcequit == 'y':
 	x = forcequit
 else:
