27fd9b300d92cc677648a996cda9568dbaa46923
Paul Bayer <paulbaye@amazon.com>
2021-06-02 23:55:09 -0400

    Resolved Issue #7

--- all_my_instances.py
+++ all_my_instances.py
@@ -33,36 +33,36 @@ parser.add_argument(
 	help="Print debugging statements - only for developers",
 	action="store_const",
 	dest="loglevel",
-	const=logging.DEBUG,	# args.loglevel = 10
-	default=logging.CRITICAL) # args.loglevel = 50
+	const=logging.DEBUG,        # args.loglevel = 10
+	default=logging.CRITICAL)   # args.loglevel = 50
 parser.add_argument(
 	'-vvv',
 	help="Print info statements - mainly for developers",
 	action="store_const",
 	dest="loglevel",
-	const=logging.INFO,	# args.loglevel = 20
-	default=logging.CRITICAL) # args.loglevel = 50
+	const=logging.INFO,         # args.loglevel = 20
+	default=logging.CRITICAL)   # args.loglevel = 50
 parser.add_argument(
 	'-vv',
 	help="Be MORE verbose",
 	action="store_const",
 	dest="loglevel",
-	const=logging.WARNING, # args.loglevel = 30
-	default=logging.CRITICAL) # args.loglevel = 50
+	const=logging.WARNING,      # args.loglevel = 30
+	default=logging.CRITICAL)   # args.loglevel = 50
 parser.add_argument(
 	'-v', '--verbose',
 	help="Be verbose",
 	action="store_const",
 	dest="loglevel",
-	const=logging.ERROR, # args.loglevel = 40
-	default=logging.CRITICAL) # args.loglevel = 50
+	const=logging.ERROR,        # args.loglevel = 40
+	default=logging.CRITICAL)   # args.loglevel = 50
 args = parser.parse_args()
 
-pProfile=args.pProfile
-pRegionList=args.pRegion
+pProfile = args.pProfile
+pRegionList = args.pRegion
 logging.basicConfig(level=args.loglevel, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
 
-EnvVars= {'Profile': os.getenv('AWS_PROFILE'),
+EnvVars = {'Profile': os.getenv('AWS_PROFILE'),
           'AccessKey': os.getenv('AWS_ACCESS_KEY_ID'),
           'SecretKey': os.getenv('AWS_SECRET_ACCESS_KEY'),
           'SessionToken': os.getenv('AWS_SESSION_TOKEN'),
@@ -70,7 +70,7 @@ EnvVars= {'Profile': os.getenv('AWS_PROFILE'),
 # Get environment variables
 
 logging.warning("EnvVars: %s" % str(EnvVars))
-account_credentials= {'Profile': None,
+account_credentials = {'Profile': None,
                       'AccessKeyId': None,
                       'SecretAccessKey': None,
                       'SessionToken': None,
@@ -91,25 +91,25 @@ User supplies ephemeral credentials (Access_Key, Secret_Key and Session Token)
 	1) Since there's a session token, we can simply use that 
 '''
 
-ProfileSupplied=False
+ProfileSupplied = False
 # They provided a profile (or more than one) at the command line
 if pProfile is not None:
 	logging.error("Using the provided profile from the command line")
-	SessionToken=False
-	ProfileSupplied=True
+	SessionToken = False
+	ProfileSupplied = True
 	pass
 # They didn't provide a profile parameter
 elif pProfile is None:
 	# They provided the profile name in the Environment Variables
 	if EnvVars['Profile'] is not None:
 		pProfile = EnvVars['Profile']
-		SessionToken=False
+		SessionToken = False
 		ProfileSupplied = True
 		logging.error("Using profile from env vars: %s", pProfile)
 	# They provided the Persistent Access Key and Secret in the Environment Variables
 	elif EnvVars['AccessKey'] is not None and EnvVars['SessionToken'] is None:
 		pProfile = EnvVars['Profile']
-		SessionToken=False
+		SessionToken = False
 		ProfileSupplied = False
 		account_credentials['AccessKeyId'] = EnvVars['AccessKey']
 		account_credentials['SecretAccessKey'] = EnvVars['SecretKey']
@@ -117,7 +117,7 @@ elif pProfile is None:
 	# They provided the ephemeral Access Key and Token in the Environment Variables
 	elif EnvVars['AccessKey'] is not None and EnvVars['SessionToken'] is not None:
 		pProfile = EnvVars['Profile']
-		SessionToken=True
+		SessionToken = True
 		ProfileSupplied = False
 		account_credentials['AccessKeyId'] = EnvVars['AccessKey']
 		account_credentials['SecretAccessKey'] = EnvVars['SecretKey']
@@ -128,53 +128,54 @@ else:
 	ProfileSupplied = False
 	print("No authentication mechanisms left!")
 	sys.exit("No authentication mechanisms left")
-
-
-
 ##########################
+
+
 ERASE_LINE = '\x1b[2K'
 
 NumInstancesFound = 0
 print()
 if ProfileSupplied:
-	fmt='%-12s %-15s %-10s %-15s %-25s %-20s %-42s %-12s'
+	fmt = '%-12s %-15s %-10s %-15s %-25s %-20s %-42s %-12s'
 	print(fmt % ("Profile", "Account #", "Region", "InstanceType", "Name", "Instance ID", "Public DNS Name", "State"))
 	print(fmt % ("-------", "---------", "------", "------------", "----", "-----------", "---------------", "-----"))
 elif not ProfileSupplied:
-	fmt='%-15s %-10s %-15s %-25s %-20s %-42s %-12s'
+	fmt = '%-15s %-10s %-15s %-25s %-20s %-42s %-12s'
 	print(fmt % ("Account #", "Region", "InstanceType", "Name", "Instance ID", "Public DNS Name", "State"))
 	print(fmt % ("---------", "------", "------------", "----", "-----------", "---------------", "-----"))
 
-RegionList=Inventory_Modules.get_regions(pRegionList)
-AllChildAccounts=[]
-SoughtAllProfiles=False
+RegionList = Inventory_Modules.get_regions(pRegionList)
+AllChildAccounts = []
+SoughtAllProfiles = False
 
 ProfileIsRoot = Inventory_Modules.find_if_org_root(pProfile)
 
 if ProfileIsRoot == 'Root':
 	logging.info("Profile %s is a Root account", pProfile)
-	Creds=Inventory_Modules.find_calling_identity(pProfile)
-	AllChildAccounts=Inventory_Modules.find_child_accounts2(pProfile)
+	Creds = Inventory_Modules.find_calling_identity(pProfile)
+	AllChildAccounts = Inventory_Modules.find_child_accounts2(pProfile)
 elif ProfileIsRoot == 'StandAlone':
 	logging.info("Profile %s is a Standalone account", pProfile)
-	Creds=Inventory_Modules.find_calling_identity(pProfile)
-	AllChildAccounts=[{
+	Creds = Inventory_Modules.find_calling_identity(pProfile)
+	AllChildAccounts = [{
 		'ParentProfile': pProfile,
 		'AccountId': Creds['AccountId'],
 		'Arn': Creds['Arn'],
+		'AccountStatus': 'ACTIVE',
 		'AccountEmail': 'noonecares@doesntmatter.com'}]
 	account_credentials['Profile'] = pProfile
 elif ProfileIsRoot == 'Child':
 	logging.info("Profile %s is a Child Account", pProfile)
-	Creds=Inventory_Modules.find_calling_identity(pProfile)
-	AllChildAccounts=[{
+	Creds = Inventory_Modules.find_calling_identity(pProfile)
+	AllChildAccounts = [{
 		'ParentProfile': pProfile,
 		'AccountId': Creds['AccountId'],
 		'Arn': Creds['Arn'],
+		'AccountStatus': 'ACTIVE',
 		'AccountEmail': 'noonecares@doesntmatter.com'}]
-	account_credentials['Profile']=pProfile
+	account_credentials['Profile'] = pProfile
 
-Instances={}
+Instances = {}
 # Removing all accounts which are SUSPENDED
 for i in reversed(range(len(AllChildAccounts))):
 	if AllChildAccounts[i]['AccountStatus'] == 'SUSPENDED':
@@ -183,20 +184,19 @@ for i in range(len(AllChildAccounts)):
 	aws_session = boto3.Session(profile_name=AllChildAccounts[i]['ParentProfile'])
 	sts_client = aws_session.client('sts')
 	logging.info("Single account record %s:", AllChildAccounts[i])
-	"""
-	TO DO - figure a way to find out whether this rolename is correct for every account?
-	"""
+	# TODO - figure a way to find out whether this rolename is correct for every account?
 	role_arn = "arn:aws:iam::{}:role/AWSCloudFormationStackSetExecutionRole".format(AllChildAccounts[i]['AccountId'])
 	logging.info("Role ARN: %s" % role_arn)
 	try:
 		# This is a standalone or child account and the profile provided is all we need
 		if len(AllChildAccounts) == 1 and pProfile == AllChildAccounts[i]['ParentProfile']:
-			continue    # We've already populated the account_credentials dict above
+			pass    # We've already populated the account_credentials dict above
 		# The profile provided is of a root account, which has access to the member accounts via some role
 		else:
-			account_credentials, role = Inventory_Modules.get_child_access2(AllChildAccounts[i]['ParentProfile'], AllChildAccounts[i]['AccountId'])
-			account_credentials['Profile']=pProfile
-		account_credentials['AccountNumber']=AllChildAccounts[i]['AccountId']
+			account_credentials, _ = Inventory_Modules.get_child_access2(AllChildAccounts[i]['ParentProfile'],
+			                                                             AllChildAccounts[i]['AccountId'])
+			account_credentials['Profile'] = pProfile
+		account_credentials['AccountNumber'] = AllChildAccounts[i]['AccountId']
 	except ClientError as my_Error:
 		if str(my_Error).find("AuthFailure") > 0:
 			logging.error("%s: Authorization Failure for account %s", AllChildAccounts[i]['ParentProfile'], AllChildAccounts[i]['AccountId'])
@@ -209,36 +209,35 @@ for i in range(len(AllChildAccounts)):
 		continue
 	for pRegion in RegionList:
 		try:
-			Instances=Inventory_Modules.find_account_instances(account_credentials, pRegion)
+			Instances = Inventory_Modules.find_account_instances(account_credentials, pRegion)
 			logging.warning("Account %s being looked at now", account_credentials['AccountNumber'])
-			InstanceNum=len(Instances['Reservations'])
+			InstanceNum = len(Instances['Reservations'])
 			print(ERASE_LINE+"Org Profile: {} Account: {} Region: {} Found {} instances".format(AllChildAccounts[i]['ParentProfile'], account_credentials['AccountNumber'], pRegion, InstanceNum), end='\r')
 		except ClientError as my_Error:
 			if str(my_Error).find("AuthFailure") > 0:
 				logging.error("Authorization Failure using {} parent profile to access {} account in {} region".format(AllChildAccounts[i]['ParentProfile'], AllChildAccounts[i]['AccountId'], pRegion))
 				logging.warning("It's possible that the region %s hasn't been opted-into", pRegion)
 				pass
-		# if len(Instances['Reservations']) > 0:
 		if 'Reservations' in Instances.keys():
 			for y in range(len(Instances['Reservations'])):
 				for z in range(len(Instances['Reservations'][y]['Instances'])):
-					InstanceType=Instances['Reservations'][y]['Instances'][z]['InstanceType']
-					InstanceId=Instances['Reservations'][y]['Instances'][z]['InstanceId']
-					PublicDnsName=Instances['Reservations'][y]['Instances'][z]['PublicDnsName']
-					State=Instances['Reservations'][y]['Instances'][z]['State']['Name']
+					InstanceType = Instances['Reservations'][y]['Instances'][z]['InstanceType']
+					InstanceId = Instances['Reservations'][y]['Instances'][z]['InstanceId']
+					PublicDnsName = Instances['Reservations'][y]['Instances'][z]['PublicDnsName']
+					State = Instances['Reservations'][y]['Instances'][z]['State']['Name']
 					# print("Length:", len(Instances['Reservations'][y]['Instances'][z]['Tags']))
+					Name = "No Name Tag"
 					try:
-						Name="No Name Tag"
 						for x in range(len(Instances['Reservations'][y]['Instances'][z]['Tags'])):
-							if Instances['Reservations'][y]['Instances'][z]['Tags'][x]['Key']=="Name":
-								Name=Instances['Reservations'][y]['Instances'][z]['Tags'][x]['Value']
-					except KeyError as my_Error:	# This is needed for when there is no "Tags" key within the describe-instances output
+							if Instances['Reservations'][y]['Instances'][z]['Tags'][x]['Key'] == "Name":
+								Name = Instances['Reservations'][y]['Instances'][z]['Tags'][x]['Value']
+					except KeyError as my_Error:    # This is needed for when there is no "Tags" key within the describe-instances output
 						logging.info(my_Error)
 						pass
 					if State == 'running':
-						fmt='%-12s %-15s %-10s %-15s %-20s %-20s %-42s '+Fore.RED+'%-12s'+Fore.RESET
+						fmt = '%-12s %-15s %-10s %-15s %-20s %-20s %-42s '+Fore.RED+'%-12s'+Fore.RESET
 					else:
-						fmt='%-12s %-15s %-10s %-15s %-20s %-20s %-42s %-12s'
+						fmt = '%-12s %-15s %-10s %-15s %-20s %-20s %-42s %-12s'
 					print(fmt % (AllChildAccounts[i]['ParentProfile'], account_credentials['AccountNumber'], pRegion, InstanceType, Name, InstanceId, PublicDnsName, State))
 					NumInstancesFound += 1
 print(ERASE_LINE)
