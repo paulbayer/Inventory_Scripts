3e8a21a5b34fc86b0214a67b12e6501f2ee22a76
Paul Bayer <paulbaye@amazon.com>
2022-03-31 18:18:14 -0400

    Updated my_ssm_parameters to use classes

--- my_ssm_parameters.py
+++ my_ssm_parameters.py
@@ -10,81 +10,48 @@ import Inventory_Modules
 import argparse
 from colorama import init, Fore, Back, Style
 from botocore.exceptions import ClientError, NoCredentialsError
+from ArgumentsClass import CommonArguments
+from account_class import aws_acct_access
 
 import logging
 
 init()
 
-parser = argparse.ArgumentParser(
-	description="We\'re going to find all SSM parameters within the master profile, and optionally delete some of them.",
-	prefix_chars='-+/')
+parser = CommonArguments()
+parser.verbosity()
+parser.singleprofile()
+parser.singleregion()
+# parser = argparse.ArgumentParser(
+# 	description="We\'re going to find all SSM parameters within the master profile, and optionally delete some of them.",
+# 	prefix_chars='-+/')
 parser.my_parser.add_argument(
-	"-p", "--profile",
-	dest="pProfile",
-	metavar="profile to use",
-	default="default",
-	help="To specify a specific profile, use this parameter. Default will be ALL profiles, including those in ~/.aws/credentials and ~/.aws/config")
+		'--ALZ',
+		help="Identify left-over parameters created by the ALZ solution",
+		action="store_const",
+		dest="ALZParam",
+		const=True,
+		default=False)
 parser.my_parser.add_argument(
-	"-r", "--region",
-	dest="pRegion",
-	metavar="region name string",
-	default="us-east-1",
-	help="String fragment of the region(s) you want to check for resources.")
+		'-b', '--daysback',
+		help="Only keep the last x days of Parameters (default 90)",
+		dest="DaysBack",
+		default=90)
 parser.my_parser.add_argument(
-	'--ALZ',
-	help="Identify left-over parameters created by the ALZ solution",
-	action="store_const",
-	dest="ALZParam",
-	const=True,
-	default=False)
-parser.my_parser.add_argument(
-	'-b', '--daysback',
-	help="Only keep the last x days of Parameters (default 90)",
-	dest="DaysBack",
-	default=90)
-parser.my_parser.add_argument(
-	'+d', '--delete',
-	help="Delete left-over parameters created by the ALZ solution",
-	action="store_const",
-	dest="DeletionRun",
-	const=True,
-	default=False)
-parser.my_parser.add_argument(
-	'-v', '--verbose',
-	help="Be verbose",
-	action="store_const",
-	dest="loglevel",
-	const=logging.ERROR,  # args.loglevel = 40
-	default=logging.CRITICAL)  # args.loglevel = 50
-parser.my_parser.add_argument(
-	'-vv',
-	help="Be MORE verbose",
-	action="store_const",
-	dest="loglevel",
-	const=logging.WARNING,  # args.loglevel = 30
-	default=logging.CRITICAL)  # args.loglevel = 50
-parser.my_parser.add_argument(
-	'-d', '--debug',
-	help="Print debugging statements",
-	action="store_const",
-	dest="loglevel",
-	const=logging.INFO,  # args.loglevel = 20
-	default=logging.CRITICAL)  # args.loglevel = 50
-parser.my_parser.add_argument(
-	'-d', '--debug',
-	help="Print LOTS of debugging statements",
-	action="store_const",
-	dest="loglevel",
-	const=logging.DEBUG,  # args.loglevel = 10
-	default=logging.CRITICAL)  # args.loglevel = 50
+		'+d', '--delete',
+		help="Delete left-over parameters created by the ALZ solution",
+		action="store_const",
+		dest="DeletionRun",
+		const=True,
+		default=False)
 args = parser.my_parser.parse_args()
 
-pProfile = args.pProfiles
-pRegion = args.pRegion
+pProfile = args.Profile
+pRegion = args.Region
 ALZParam = args.ALZParam
 DeletionRun = args.DeletionRun
 dtDaysBack = datetime.timedelta(days=int(args.DaysBack))
-logging.basicConfig(level=args.loglevel, format="[%(filename)s:%(lineno)s:%(levelname)s - %(funcName)20s() ] %(message)s")
+logging.basicConfig(level=args.loglevel,
+                    format="[%(filename)s:%(lineno)s:%(levelname)s - %(funcName)20s() ] %(message)s")
 
 ##########################
 ERASE_LINE = '\x1b[2K'
@@ -104,7 +71,7 @@ try:
 	# Since there could be 10,000 parameters stored in the Parameter Store - this function COULD take a long time
 	Parameters = Inventory_Modules.find_ssm_parameters(pProfile, pRegion)
 	logging.error("Profile: %s found a total of %s parameters", pProfile, len(Parameters))
-	# print(ERASE_LINE,"Account:",account['AccountId'],"Found",len(Users),"users",end='\r')
+# print(ERASE_LINE,"Account:",account['AccountId'],"Found",len(Users),"users",end='\r')
 except ClientError as my_Error:
 	if str(my_Error).find("AuthFailure") > 0:
 		print(f"{pProfile}: Authorization Failure")
@@ -115,39 +82,40 @@ if ALZParam:
 		# If the parameter matches the string regex of "/2ac07efd-153d-4069-b7ad-0d18cc398b11/105" - then it should be a candidate for deletion
 		# With Regex - I'm looking for "/\w{8,8}-\w{4,4}-\w{4,4}-\w{4,4}-\w{12,12}/\w{3,3}"
 		ParameterDate = Parameters[y]['LastModifiedDate']
-		mydelta = today-ParameterDate  # this is a "timedelta" object
+		mydelta = today - ParameterDate  # this is a "timedelta" object
 		p = re.compile(ALZRegex)  # Sets the regex to look for
 		logging.info("Parameter %s: %s with date %s", y, Parameters[y]['Name'], Parameters[y]['LastModifiedDate'])
 		if p.match(Parameters[y]['Name']) and mydelta > dtDaysBack:
-			logging.error("Parameter %s with date of %s matched", Parameters[y]['Name'], Parameters[y]['LastModifiedDate'])
+			logging.error("Parameter %s with date of %s matched", Parameters[y]['Name'],
+			              Parameters[y]['LastModifiedDate'])
 			ALZParams += 1
 			if DeletionRun:
 				ParamsToDelete.append(Parameters[y]['Name'])
 if DeletionRun:
-	print("Deleting {} ALZ-related Parameters now, further back than {} days".format(len(ParamsToDelete), dtDaysBack.days))
+	print(f"Deleting {len(ParamsToDelete)} ALZ-related Parameters now, further back than {dtDaysBack.days} days")
 	# for i in range(len(ParamsToDelete)):
 	mark = 0
 	i = 0
-	while i < len(ParamsToDelete)+1:
+	while i < len(ParamsToDelete) + 1:
 		i += 1
 		if i % 10 == 0:
 			response = client_ssm.delete_parameters(
-				Names=ParamsToDelete[mark:i]
-			)
+					Names=ParamsToDelete[mark:i]
+					)
 			mark = i
 			print(ERASE_LINE, f"{i} parameters deleted and {len(ParamsToDelete) - i} more to go...", end='\r')
 		elif i == len(ParamsToDelete):
 			response = client_ssm.delete_parameters(
-				Names=ParamsToDelete[mark:i]
-			)
-			logging.warning("Deleted the last %s parameters.", i%10)
+					Names=ParamsToDelete[mark:i]
+					)
+			logging.warning(f"Deleted the last {i} parameters.")
 print()
 print(ERASE_LINE)
-print("Found {} total parameters".format(len(Parameters)))
+print(f"Found {len(Parameters)} total parameters")
 if ALZParam:
 	print(f"And {ALZParams} of them were from buggy ALZ runs more than {dtDaysBack.days} days back")
 if DeletionRun:
-	print("And we deleted {} of them".format(len(ParamsToDelete)))
+	print(f"And we deleted {len(ParamsToDelete)} of them")
 print()
 print("Thanks for using this script.")
 print()
