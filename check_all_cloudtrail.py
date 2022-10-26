51b89515aa6fa0bd3613ed5faf04cfffe39c4875
Paul Bayer <paulbaye@amazon.com>
2022-10-26 13:26:54 -0400

    Added skip account function and enhanced the summary table at the end

--- check_all_cloudtrail.py
+++ check_all_cloudtrail.py
@@ -6,7 +6,7 @@ from ArgumentsClass import CommonArguments
 from account_class import aws_acct_access
 from colorama import init, Fore
 from botocore.exceptions import ClientError
-# from prettytable import PrettyTable
+from prettytable import PrettyTable
 
 import logging
 
@@ -15,11 +15,15 @@ init()
 parser = CommonArguments()
 parser.multiprofile()
 parser.multiregion()
+parser.extendedargs()
+parser.rootOnly()
 parser.verbosity()
 args = parser.my_parser.parse_args()
 
 pProfiles = args.Profiles
 pRegionList = args.Regions
+pSkipAccounts = args.SkipAccounts
+pRootOnly = args.RootOnly
 verbose = args.loglevel
 logging.basicConfig(level=args.loglevel, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
 
@@ -40,13 +44,17 @@ def check_accounts_for_cloudtrail(faws_acct, fRegionList=None):
 	AllTrails = []
 	account_credentials = {'Role': 'Nothing'}
 	Trails = dict()
+	AccountNum = 0
 	if fRegionList is None:
 		fRegionList = ['us-east-1']
 	for account in ChildAccounts:
-		SkipAccounts = ['135034107635', '177672183868', '382978328952']
+		SkipAccounts = pSkipAccounts
 		if account['AccountId'] in SkipAccounts:
 			continue
+		elif pRootOnly and not account['AccountId'] == account['MgmtAccount']:
+			continue
 		logging.info(f"Connecting to account {account['AccountId']}")
+		AccountNum += 1
 		try:
 			account_credentials = Inventory_Modules.get_child_access3(faws_acct, account['AccountId'])
 			logging.info(f"Connected to account {account['AccountId']} using role {account_credentials['Role']}")
@@ -63,13 +71,15 @@ def check_accounts_for_cloudtrail(faws_acct, fRegionList=None):
 			continue
 		except KeyError as my_Error:
 			logging.error(f"Account Access failed - trying to access {account['AccountId']}")
+			logging.info(f"Actual Error: {my_Error}")
+			pass
 		except AttributeError as my_Error:
 			logging.error(f"Error: Likely that one of the supplied profiles {pProfiles} was wrong")
 			logging.warning(my_Error)
 			continue
 		for region in fRegionList:
 			try:
-				print(f"{ERASE_LINE}Checking account {account['AccountId']} in region {region}", end='\r')
+				print(f"{ERASE_LINE}{AccountNum} / {len(ChildAccounts)}: Checking account {account['AccountId']} in region {region}", end='\r')
 				Trails = Inventory_Modules.find_account_cloudtrail2(account_credentials, region)
 				logging.info(f"Root Account: {faws_acct.acct_number} Account: {account['AccountId']} Region: {region} | Found {len(Trails['trailList'])} trails")
 			except ClientError as my_Error:
@@ -84,7 +94,7 @@ def check_accounts_for_cloudtrail(faws_acct, fRegionList=None):
 					Trails['trailList'][y]['Region'] = region
 					TrailName = Trails['trailList'][y]['Name']
 					MultiRegion = Trails['trailList'][y]['IsMultiRegionTrail']
-					OrgTrail = Trails['trailList'][y]['IsOrganizationTrail']
+					Trails['trailList'][y]['OrgTrail'] = "OrgTrail" if Trails['trailList'][y]['IsOrganizationTrail'] else "Account Trail"
 					Bucket = Trails['trailList'][y]['S3BucketName']
 					KMS = Trails['trailList'][y]['KmsKeyId'] if 'KmsKeyId' in Trails.keys() else None
 					Trails['trailList'][y]['KMS'] = KMS
@@ -96,7 +106,7 @@ def check_accounts_for_cloudtrail(faws_acct, fRegionList=None):
 					Trails['trailList'][y]['SNSTopicName'] = SNSTopicName
 					# fmt = '%-12s %-12s %-10s %-15s %-20s %-20s %-12s'
 					# print(fmt % (faws_acct.acct_number, account['AccountId'], region, InstanceType, Name, Engine, State))
-					print(f"{faws_acct.acct_number:12s} {account['AccountId']:12s} {region:15s} {TrailName:40s} {str(OrgTrail):15s} {Bucket:45s} ")
+					print(f"{faws_acct.acct_number:12s} {account['AccountId']:12s} {region:15s} {TrailName:40s} {Trails['trailList'][y]['OrgTrail']:15s} {Bucket:45s} ")
 			AllTrails.extend(Trails['trailList'])
 	return (AllTrails)
 
@@ -136,30 +146,31 @@ else:
 
 ChildAccountList = [[item['MgmtAccount'], item['AccountId']] for item in AllChildAccounts]
 ChildAccountsWithCloudTrail = [[item['MgmtAccount'], item['AccountId']] for item in TrailsFound]
-ProblemAccounts = [item['AccountId'] for item in ChildAccountList if item not in ChildAccountsWithCloudTrail]
+ProblemAccounts = [item[1] for item in ChildAccountList if item not in ChildAccountsWithCloudTrail]
 
 print(ERASE_LINE)
-# if verbose < 50:
-# 	print(f"This table represents the summary of CloudTrail within the Org:")
-# 	x = PrettyTable()
-# 	x.field_names = ['Root Account', 'Account', 'Region', 'CloudTrail Name', 'Org Trail', 'Bucket Name']
-#
-# 	for item in ChildAccountList:
-# 		MgmtAccount = item[0]
-# 		ChildAccount = item[1]
-# 		for region in RegionList:
-# 			trailFound = False
-# 			for trail in TrailsFound:
-# 				if trail['AccountId'] == ChildAccount and trail['Region'] == region:
-# 					x.add_row([trail['MgmtAccount'], trail['AccountId'], region, trail['Name'], trail['IsOrganizationTrail'], trail['S3BucketName']])
-# 					trailFound = True
-# 				elif trailFound:
-# 					pass
-# 				else:
-# 					x.add_row([MgmtAccount, ChildAccount, region, 'None', 'None', 'None'])
-# 	print()
-# 	print(x)
-print(f"Problem Accounts: {ProblemAccounts}")
+if verbose < 50:
+	print(f"This table represents the summary of CloudTrail within the Org:")
+	x = PrettyTable()
+	x.field_names = ['Root Account', 'Account', 'Region', 'CloudTrail Name', 'Org Trail', 'Bucket Name']
+	x.align["CloudTrail Name"] = "l"
+	x.align["Bucket Name"] = "l"
+	x.align["Org Trail"] = "l"
+
+	for item in ChildAccountList:
+		MgmtAccount = item[0]
+		ChildAccount = item[1]
+		for region in RegionList:
+			trailFound = False
+			for trail in TrailsFound:
+				if trail['AccountId'] == ChildAccount and trail['Region'] == region:
+					x.add_row([trail['MgmtAccount'], trail['AccountId'], region, trail['Name'], trail['OrgTrail'], trail['S3BucketName']])
+					trailFound = True
+			if not trailFound:
+				x.add_row([MgmtAccount, ChildAccount, region, 'None', 'None', 'None'])
+	print()
+	print(x)
+print(f"Problem Accounts: {[acct for acct in ProblemAccounts if acct not in pSkipAccounts]}")
 print()
 print(f"Found {len(TrailsFound)} trails across {len(AllChildAccounts)} accounts across {len(RegionList)} regions")
 print()
