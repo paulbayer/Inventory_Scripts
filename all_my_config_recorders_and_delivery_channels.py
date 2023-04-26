99ab65166566216cb0593c0d874b54041f851ebe
Paul Bayer <paulbaye@amazon.com>
2023-04-26 19:51:52 -0400

    Enabled fragment finding and deleting

--- all_my_config_recorders_and_delivery_channels.py
+++ all_my_config_recorders_and_delivery_channels.py
@@ -2,9 +2,8 @@
 
 import sys
 import Inventory_Modules
-from Inventory_Modules import get_credentials_for_accounts_in_org, display_results, get_all_credentials
+from Inventory_Modules import display_results, get_all_credentials
 from ArgumentsClass import CommonArguments
-from account_class import aws_acct_access
 from colorama import init, Fore
 from botocore.exceptions import ClientError
 from queue import Queue
@@ -24,6 +23,8 @@ parser.multiprofile()
 parser.multiregion()
 parser.extendedargs()  # This adds additional *optional* arguments to the listing
 parser.rootOnly()
+parser.timing()
+parser.fragment()
 parser.verbosity()
 parser.my_parser.add_argument(
 	"+delete", "+forreal",
@@ -35,6 +36,7 @@ args = parser.my_parser.parse_args()
 pProfiles = args.Profiles
 pRegionList = args.Regions
 pAccounts = args.Accounts
+pFragments = args.Fragments
 pSkipAccounts = args.SkipAccounts
 pSkipProfiles = args.SkipProfiles
 pRootOnly = args.RootOnly
@@ -46,7 +48,7 @@ logging.basicConfig(level=args.loglevel, format="[%(filename)s:%(lineno)s - %(fu
 
 
 ##########################
-def check_accounts_for_delivery_channels_and_config_recorders(CredentialList, fRegionList=None, fFixRun=False):
+def check_accounts_for_delivery_channels_and_config_recorders(CredentialList, fRegionList=None, fFragments=None, fFixRun=False):
 	"""
 	Note that this function takes a list of Credentials and checks for config recorder and delivery channel in every account it has creds for
 	"""
@@ -60,34 +62,63 @@ def check_accounts_for_delivery_channels_and_config_recorders(CredentialList, fR
 		def run(self):
 			while True:
 				# Get the work from the queue and expand the tuple
-				c_account_credentials, c_fixrun, c_PlacesToLook, c_PlaceCount = self.queue.get()
+				c_account_credentials, c_fixrun, c_fragments, c_PlacesToLook, c_PlaceCount = self.queue.get()
 				logging.info(f"De-queued info for account {c_account_credentials['AccountId']} in region {c_account_credentials['Region']}")
 				try:
 					logging.info(f"Checking for config recorders and delivery channels in account {c_account_credentials['AccountId']} in region {c_account_credentials['Region']}")
+					capture_this_delivery_channel = False
 					account_dcs = Inventory_Modules.find_delivery_channels2(c_account_credentials, c_account_credentials['Region'])
 					if len(account_dcs['DeliveryChannels']) > 0:
-						account_dcs['DeliveryChannels'][0].update({'Type'           : 'Delivery Channel',
-																   'AccountId'      : c_account_credentials['AccountNumber'],
-																   'AccessKeyId'    : c_account_credentials['AccessKeyId'],
-																   'SecretAccessKey': c_account_credentials['SecretAccessKey'],
-																   'SessionToken'   : c_account_credentials['SessionToken'],
-																   'Region'         : c_account_credentials['Region'],
-																   'MgmtAccount'    : c_account_credentials['MgmtAccount'],
-																   'ParentProfile'  : c_account_credentials['ParentProfile'],
-																   'Deleted'        : False})
-						account_crs_and_dcs.extend(account_dcs['DeliveryChannels'])
+						if c_fragments is None or 'all' in c_fragments:
+							capture_this_delivery_channel = True
+							logging.info(f"No fragment provided. Found {account_dcs['DeliveryChannels'][0]['name']}")
+						else:
+							for fragment in c_fragments:
+								if fragment in account_dcs['DeliveryChannels'][0]['name']:
+									capture_this_delivery_channel = True
+									logging.info(f"Found {account_dcs['DeliveryChannels'][0]['name']} which contains {fragment}")
+									break
+								else:
+									capture_this_delivery_channel = False
+									logging.info(f"Looking for {fragment}. Found {account_dcs['DeliveryChannels'][0]['name']}, so skipping...")
+						if capture_this_delivery_channel:
+							account_dcs['DeliveryChannels'][0].update({'Type'           : 'Delivery Channel',
+							                                           'AccountId'      : c_account_credentials['AccountNumber'],
+							                                           'AccessKeyId'    : c_account_credentials['AccessKeyId'],
+							                                           'SecretAccessKey': c_account_credentials['SecretAccessKey'],
+							                                           'SessionToken'   : c_account_credentials['SessionToken'],
+							                                           'Region'         : c_account_credentials['Region'],
+							                                           'MgmtAccount'    : c_account_credentials['MgmtAccount'],
+							                                           'ParentProfile'  : c_account_credentials['ParentProfile'],
+							                                           'Deleted'        : False})
+							account_crs_and_dcs.extend(account_dcs['DeliveryChannels'])
+
 					account_crs = Inventory_Modules.find_config_recorders2(c_account_credentials, c_account_credentials['Region'])
+					capture_this_config_recorder = False
 					if len(account_crs['ConfigurationRecorders']) > 0:
-						account_crs['ConfigurationRecorders'][0].update({'Type'           : 'Config Recorder',
-																		 'AccountId'      : c_account_credentials['AccountNumber'],
-																		 'AccessKeyId'    : c_account_credentials['AccessKeyId'],
-																		 'SecretAccessKey': c_account_credentials['SecretAccessKey'],
-																		 'SessionToken'   : c_account_credentials['SessionToken'],
-																		 'Region'         : c_account_credentials['Region'],
-																		 'MgmtAccount'    : c_account_credentials['MgmtAccount'],
-																		 'ParentProfile'  : c_account_credentials['ParentProfile'],
-																		 'Deleted'        : False})
-						account_crs_and_dcs.extend(account_crs['ConfigurationRecorders'])
+						if c_fragments is None or 'all' in c_fragments:
+							capture_this_config_recorder = True
+							logging.info(f"No fragment provided. Found {account_crs['ConfigurationRecorders'][0]['name']}")
+						else:
+							for fragment in c_fragments:
+								if fragment in account_crs['ConfigurationRecorders'][0]['name']:
+									capture_this_config_recorder = True
+									logging.info(f"Found {account_crs['ConfigurationRecorders'][0]['name']} which contains {fragment}")
+									break
+								else:
+									capture_this_config_recorder = False
+									logging.info(f"Looking for {fragment}. Found {account_crs['ConfigurationRecorders'][0]['name']}, so skipping...")
+						if capture_this_config_recorder:
+							account_crs['ConfigurationRecorders'][0].update({'Type'           : 'Config Recorder',
+																			 'AccountId'      : c_account_credentials['AccountNumber'],
+																			 'AccessKeyId'    : c_account_credentials['AccessKeyId'],
+																			 'SecretAccessKey': c_account_credentials['SecretAccessKey'],
+																			 'SessionToken'   : c_account_credentials['SessionToken'],
+																			 'Region'         : c_account_credentials['Region'],
+																			 'MgmtAccount'    : c_account_credentials['MgmtAccount'],
+																			 'ParentProfile'  : c_account_credentials['ParentProfile'],
+																			 'Deleted'        : False})
+							account_crs_and_dcs.extend(account_crs['ConfigurationRecorders'])
 					logging.info(f"Successfully connected to account {c_account_credentials['AccountId']} in region {c_account_credentials['Region']}")
 				except KeyError as my_Error:
 					logging.error(f"Account Access failed - trying to access {c_account_credentials['AccountId']} in region {c_account_credentials['Region']}")
@@ -123,7 +154,7 @@ def check_accounts_for_delivery_channels_and_config_recorders(CredentialList, fR
 		try:
 			# print(f"{ERASE_LINE}Queuing account {credential['AccountId']} in region {credential['Region']}", end='\r')
 			# I don't know why - but double parens are necessary below. If you remove them, only the first parameter is queued.
-			checkqueue.put((credential, fFixRun, len(CredentialList), PlaceCount))
+			checkqueue.put((credential, fFixRun, fFragments, len(CredentialList), PlaceCount))
 			PlaceCount += 1
 		except ClientError as my_Error:
 			if str(my_Error).find("AuthFailure") > 0:
@@ -148,36 +179,9 @@ display_dict = {'ParentProfile': {'Format': '24s', 'DisplayOrder': 1, 'Heading':
 
 NumObjectsFound = 0
 NumAccountsInvestigated = 0
-# AllCredentials = []
-RegionList = ['us-east-1']
-
-# if pProfiles is None:  # Default use case from the classes
-# 	print("Using the default profile - gathering info")
-# 	aws_acct = aws_acct_access()
-# 	RegionList = Inventory_Modules.get_regions3(aws_acct, pRegionList)
-# 	# This should populate the list "AllCreds" with the credentials for the relevant accounts.
-# 	logging.info(f"Queueing default profile for credentials")
-# 	profile = 'default'
-# 	AllCredentials.extend(get_credentials_for_accounts_in_org(aws_acct, pSkipAccounts, pRootOnly, pAccounts, profile, RegionList))
-# else:
-# 	ProfileList = Inventory_Modules.get_profiles(fSkipProfiles=pSkipProfiles, fprofiles=pProfiles)
-# 	print(f"Capturing info for {len(ProfileList)} requested profiles {ProfileList}")
-# 	for profile in ProfileList:
-# 		# Eventually - getting credentials for a single account may require passing in the region in which it's valid, but not yet.
-# 		try:
-# 			aws_acct = aws_acct_access(profile)
-# 			print(f"Validating {len(aws_acct.ChildAccounts)} accounts within {profile} profile now... ")
-# 			RegionList = Inventory_Modules.get_regions3(aws_acct, pRegionList)
-# 			logging.info(f"Queueing {profile} for credentials")
-# 			# This should populate the list "AllCredentials" with the credentials for the relevant accounts.
-# 			AllCredentials.extend(get_credentials_for_accounts_in_org(aws_acct, pSkipAccounts, pRootOnly, pAccounts, profile, RegionList))
-# 		except AttributeError as my_Error:
-# 			logging.error(f"Profile {profile} didn't work... Skipping")
-# 			continue
 
 AllCredentials = get_all_credentials(pProfiles, pTiming, pSkipProfiles, pSkipAccounts, pRootOnly, pAccounts, pRegionList)
 RegionList = list(set([x['Region'] for x in AllCredentials]))
-
 AccountNum = len(set([acct['AccountId'] for acct in AllCredentials]))
 
 cf_regions = Inventory_Modules.get_service_regions('config', RegionList)
@@ -189,7 +193,7 @@ if pTiming:
 	print(f"{Fore.GREEN}\t\tFiguring out what regions are available to your accounts, and capturing credentials for all accounts in those regions took: {(milestone_time1 - begin_time):.3f} seconds{Fore.RESET}")
 	print()
 print(f"Now running through all accounts and regions identified to find resources...")
-all_config_recorders_and_delivery_channels = check_accounts_for_delivery_channels_and_config_recorders(AllCredentials, cf_regions, DeletionRun)
+all_config_recorders_and_delivery_channels = check_accounts_for_delivery_channels_and_config_recorders(AllCredentials, cf_regions, pFragments, DeletionRun)
 
 if pTiming:
 	print()
