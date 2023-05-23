ea6ec41cb1aa1d1b9eaf5b74cca37cfdd8a3d5ce
Paul Bayer <paulbaye@amazon.com>
2023-05-23 16:52:29 -0400

    Updated to include OU policies

--- DrawOrg.py
+++ DrawOrg.py
@@ -20,19 +20,25 @@ logging.basicConfig(level=verbose, format="[%(filename)s:%(lineno)s - %(funcName
 org_client = boto3.Session(profile_name=pProfile).client('organizations')
 
 root = org_client.list_roots()['Roots'][0]['Id']
-
+account_fillcolor = 'orange'
+account_shape = 'ellipse'
+policy_fillcolor = 'azure'
+policy_linecolor = 'red'
+policy_shape = 'hexagon'
+ou_fillcolor = 'burlywood'
+ou_shape = 'box'
 
 def get_root_OUS(root_id):
 	try:
 		ChildOUs = org_client.list_children(ParentId=root_id, ChildType='ORGANIZATIONAL_UNIT')
 	except (org_client.exceptions.AccessDeniedException,
-			org_client.exceptions.AWSOrganizationsNotInUseException,
-			org_client.exceptions.InvalidInputException,
-			org_client.exceptions.ParentNotFoundException,
-			org_client.exceptions.ServiceException,
-			org_client.exceptions.TooManyRequestsException) as myError:
+	        org_client.exceptions.AWSOrganizationsNotInUseException,
+	        org_client.exceptions.InvalidInputException,
+	        org_client.exceptions.ParentNotFoundException,
+	        org_client.exceptions.ServiceException,
+	        org_client.exceptions.TooManyRequestsException) as myError:
 		logging.error(f"Error: {myError}")
-	return(ChildOUs['Children'])
+	return (ChildOUs['Children'])
 
 
 # Function to recursively traverse the OUs and accounts
@@ -41,18 +47,43 @@ def traverse_ous_and_accounts(ou_id, dot):
 	ou = org_client.describe_organizational_unit(OrganizationalUnitId=ou_id)
 	ou_name = ou['OrganizationalUnit']['Name']
 
+	# Retrieve the policies associated with this OU
+	associated_scps = org_client.list_policies_for_target(TargetId=ou_id, Filter='SERVICE_CONTROL_POLICY')['Policies']
+	for policy in associated_scps:
+		policy_id = policy['Id']
+		policy_name = policy['Name']
+		"""
+		Possible values as of 5/23/2023
+		AISERVICES_OPT_OUT_POLICY
+		BACKUP_POLICY
+        SERVICE_CONTROL_POLICY
+        TAG_POLICY
+		"""
+		if policy['Type'] == 'SERVICE_CONTROL_POLICY':
+			policy_type = 'scp'
+		elif policy['Type'] == 'TAG_POLICY':
+			policy_type = 'tag'
+		elif policy['Type'] == 'BACKUP_POLICY':
+			policy_type = 'backup'
+		elif policy['Type'] == 'AISERVICES_OPT_OUT_POLICY':
+			policy_type = 'ai'
+		else:
+			policy_type = 'unknown'
+		dot.node(policy_id, label=f"{policy_name}\n {policy_id} | {policy_type}", shape=policy_shape, color=policy_linecolor, style='filled', fillcolor=policy_fillcolor)
+		dot.edge(ou_id, policy_id)
+
 	# Retrieve the accounts under the current OU
 	accounts = org_client.list_accounts_for_parent(ParentId=ou_id)
 
 	# Add the current OU as a node in the diagram
-	dot.node(ou_id, label=f"{ou_name} | {len(accounts['Accounts'])} ", shape='box', style='filled', fillcolor='darkgrey')
+	dot.node(ou_id, label=f"{ou_name} | {len(accounts['Accounts'])}\n{ou_id}", shape=ou_shape, style='filled', fillcolor=ou_fillcolor)
 
 	for account in accounts['Accounts']:
 		account_id = account['Id']
 		account_name = account['Name']
 
 		# Add the account as a node in the diagram
-		dot.node(account_id, label=f"{account_name}\n{account_id}", shape='ellipse')
+		dot.node(account_id, label=f"{account_name}\n{account_id}", shape=account_shape, style='filled', fillcolor=account_fillcolor)
 
 		# Add an edge from the current OU to the account
 		dot.edge(ou_id, account_id)
