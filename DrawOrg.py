bccf70aeff4b511f438332eee9196ee81785ae6b
Paul Bayer <paulbaye@amazon.com>
2023-10-03 18:00:49 -0400

    Updated to conform to new Python style

--- DrawOrg.py
+++ DrawOrg.py
@@ -1,3 +1,5 @@
+import sys
+
 import boto3
 import logging
 from graphviz import Digraph
@@ -5,44 +7,9 @@ from time import time
 from colorama import init, Fore
 from ArgumentsClass import CommonArguments
 
-__version__ = '2023.05.31'
+__version__ = '2023.10.03'
 init()
 
-parser = CommonArguments()
-parser.my_parser.description = ("To draw the Organization and its policies.")
-parser.singleprofile()
-parser.verbosity()
-parser.timing()
-parser.version(__version__)
-parser.my_parser.add_argument(
-	"--policy",
-	dest='policy',
-	action="store_true",  # Defaults to False, meaning it won't show policies by default
-	help="Only run this code for the root account, not the children")
-parser.my_parser.add_argument(
-	"--aws", "--managed",
-	dest='aws_managed',
-	action="store_true",  # Defaults to False, meaning it defaults to NOT showing the AWS managed policies applied
-	help="Use this parameter to SHOW the AWS Managed SCPs as well, otherwise they're hidden")
-args = parser.my_parser.parse_args()
-
-pProfile = args.Profile
-pTiming = args.Time
-pPolicy = args.policy
-pManaged = args.aws_managed
-verbose = args.loglevel
-logging.basicConfig(level=verbose, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(""message)s")
-
-if pTiming:
-	begin_time = time()
-print(f"Beginning to look through the Org in order to create the diagram")
-# Create an AWS Organizations client
-org_session = boto3.Session(profile_name=pProfile)
-org_client = org_session.client('organizations')
-ERASE_LINE = '\x1b[2K'
-
-root = org_client.list_roots()['Roots'][0]['Id']
-max_accounts_per_ou = 1
 account_fillcolor = 'orange'
 account_shape = 'ellipse'
 policy_fillcolor = 'azure'  # Pretty color - nothing to do with the Azure Cloud...
@@ -51,18 +18,30 @@ policy_shape = 'hexagon'
 ou_fillcolor = 'burlywood'
 ou_shape = 'box'
 
-"""
-Possible Org policy values as of 5/23/2023
-AISERVICES_OPT_OUT_POLICY
-BACKUP_POLICY
-SERVICE_CONTROL_POLICY
-TAG_POLICY
-"""
-aws_policy_type_list = ['SERVICE_CONTROL_POLICY', 'TAG_POLICY', 'BACKUP_POLICY', 'AISERVICES_OPT_OUT_POLICY']
 #####################
 """
 Function Definitions
 """
+
+
+def parse_args(args):
+	parser = CommonArguments()
+	parser.my_parser.description = ("To draw the Organization and its policies.")
+	parser.singleprofile()
+	parser.verbosity()
+	parser.timing()
+	parser.version(__version__)
+	parser.my_parser.add_argument(
+		"--policy",
+		dest='policy',
+		action="store_true",  # Defaults to False, meaning it won't show policies by default
+		help="Only run this code for the root account, not the children")
+	parser.my_parser.add_argument(
+		"--aws", "--managed",
+		dest='aws_managed',
+		action="store_true",  # Defaults to False, meaning it defaults to NOT showing the AWS managed policies applied
+		help="Use this parameter to SHOW the AWS Managed SCPs as well, otherwise they're hidden")
+	return (parser.my_parser.parse_args(args))
 
 
 def round_up(number):
@@ -129,16 +108,17 @@ def traverse_ous_and_accounts(ou_id, dot):
 		# Add an edge from the current OU to the account
 		dot.edge(ou_id, account_id)
 
+		# TODO: Would love to multi-thread this... but we'll run into API limits quickly.
 		if pPolicy:
 			# Gather every kind of policy that could be attached to an account
 			for aws_policy_type in aws_policy_type_list:
 				logging.info(f"Checking for {aws_policy_type} policies on account {account_id}")
 				account_associated_policies.extend(org_client.list_policies_for_target(TargetId=account_id, Filter=aws_policy_type)['Policies'])
 			# Create a list of policy associations with the account that's connected to them
-			all_account_associated_policies.extend([{'AcctId': account_id,
-			                                         'PolicyId': x['Id'],
-			                                         'PolicyName':x['Name'],
-			                                         'PolicyType': x['Type'],
+			all_account_associated_policies.extend([{'AcctId'     : account_id,
+			                                         'PolicyId'   : x['Id'],
+			                                         'PolicyName' : x['Name'],
+			                                         'PolicyType' : x['Type'],
 			                                         'AWS_Managed': x['AwsManaged']} for x in account_associated_policies])
 
 	if pPolicy:
@@ -158,7 +138,6 @@ def traverse_ous_and_accounts(ou_id, dot):
 		child_ou_id = child_ou['Id']
 
 		# Recursively traverse the child OU and add edges to the diagram
-		# max_accounts_per_ou = max(len(all_accounts), max_accounts_per_ou)
 		traverse_ous_and_accounts(child_ou_id, dot)
 		dot.edge(ou_id, child_ou_id)
 
@@ -166,7 +145,11 @@ def traverse_ous_and_accounts(ou_id, dot):
 def create_policy_nodes(dot):
 	associated_policies = []
 	for aws_policy_type in aws_policy_type_list:
-		associated_policies.extend(org_client.list_policies(Filter=aws_policy_type)['Policies'])
+		response = org_client.list_policies(Filter=aws_policy_type)
+		associated_policies.extend(response['Policies'])
+		while 'NextToken' in response.keys():
+			response = org_client.list_policies(Filter=aws_policy_type, NextToken=response['NextToken'])
+			associated_policies.extend(response['Policies'])
 	for policy in associated_policies:
 		policy_id = policy['Id']
 		policy_name = policy['Name']
@@ -198,7 +181,7 @@ def find_max_accounts_per_ou(ou_id, max_accounts=0):
 		all_accounts.extend(accounts['Accounts'])
 		logging.info(f"Found {len(all_accounts)} more accounts in ou {ou_id} - totaling {len(all_accounts)} accounts so far")
 	max_accounts = max(len(all_accounts), max_accounts)
-	return(max_accounts)
+	return (max_accounts)
 
 
 def find_accounts_in_org():
@@ -209,40 +192,79 @@ def find_accounts_in_org():
 		org_accounts = org_client.list_accounts(NextToken=org_accounts['NextToken'])
 		all_accounts.extend(org_accounts['Accounts'])
 		logging.info(f"Finding another {len(org_accounts['Accounts'])}. Total accounts found: {len(all_accounts)}")
-	return(all_accounts)
+	return (all_accounts)
+
+
+def draw_org(froot_OUs):
+	max_accounts_per_ou = 1
+
+	# Create a new Digraph object for the diagram
+	dot = Digraph('AWS Organization', format='png', comment="Organization Structure")
+	# dot = dot_wide.unflatten(stagger=round_up(len(root_OUs)/5))
+
+	if pPolicy:
+		create_policy_nodes(dot)
+	# Call the function to traverse the OUs and accounts starting from the root
+	print(f"Beginning to traverse OUs and draw the diagram... ")
+	for ou in froot_OUs:
+		traverse_ous_and_accounts(ou['Id'], dot)
+		max_accounts_per_ou = find_max_accounts_per_ou(ou['Id'], max_accounts_per_ou)
+	if pTiming and pPolicy:
+		print(f"{Fore.GREEN}\tDrawing the Org structure when policies are included took {time() - begin_time:.2f} seconds{Fore.RESET}")
+	elif pTiming:
+		print(f"{Fore.GREEN}\tDrawing the Org structure without policies took {time() - begin_time:.2f} seconds{Fore.RESET}")
+	# Render the diagram to a PNG image
+	# dot.render('aws_organization', view=True)
+	dot_unflat = dot.unflatten(stagger=round_up(max_accounts_per_ou / 5))
+	dot_unflat.render('aws_organization', view=True)
 
 
 #####################################
-# Specify the AWS root organization ID
-all_org_accounts = find_accounts_in_org()
-if pPolicy:
-	print(f"Due to there being {len(all_org_accounts)} accounts in this Org, this process will likely take about {5+(len(all_org_accounts)/10)} seconds")
-else:
-	print(f"Due to there being {len(all_org_accounts)} accounts in this Org, this process will likely take about {5+(len(all_org_accounts)/10)} seconds")
-root_OUs = get_root_OUS(root)
 
-# Create a new Digraph object for the diagram
-dot = Digraph('AWS Organization', format='png', comment="Organization Structure")
-# dot = dot_wide.unflatten(stagger=round_up(len(root_OUs)/5))
 
-if pPolicy:
-	create_policy_nodes(dot)
-# Call the function to traverse the OUs and accounts starting from the root
-print(f"Beginning to traverse OUs and draw the diagram... ")
-for ou in root_OUs:
-	traverse_ous_and_accounts(ou['Id'], dot)
-	max_accounts_per_ou = find_max_accounts_per_ou(ou['Id'], max_accounts_per_ou)
-if pTiming and pPolicy:
-	print(f"{Fore.GREEN}\tDrawing the Org structure when policies are included took {time()-begin_time:.2f} seconds{Fore.RESET}")
-elif pTiming:
-	print(f"{Fore.GREEN}\tDrawing the Org structure without policies took {time()-begin_time:.2f} seconds{Fore.RESET}")
-# Render the diagram to a PNG image
-# dot.render('aws_organization', view=True)
-dot_unflat = dot.unflatten(stagger=round_up(max_accounts_per_ou/5))
-dot_unflat.render('aws_organization', view=True)
+if __name__ == '__main__':
+	args = parse_args(sys.argv[1:])
 
-if pTiming:
-	print(f"{Fore.GREEN}\tThis script took {time()-begin_time:.2f} seconds{Fore.RESET}")
+	pProfile = args.Profile
+	pTiming = args.Time
+	pPolicy = args.policy
+	pManaged = args.aws_managed
+	verbose = args.loglevel
+	logging.basicConfig(level=verbose, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(""message)s")
+
+	if pTiming:
+		begin_time = time()
+	print(f"Beginning to look through the Org in order to create the diagram")
+	# Create an AWS Organizations client
+	org_session = boto3.Session(profile_name=pProfile)
+	org_client = org_session.client('organizations')
+	ERASE_LINE = '\x1b[2K'
+
+	# Find the root Org ID
+	root = org_client.list_roots()['Roots'][0]['Id']
+
+	"""
+	Possible Org policy values as of 5/23/2023
+	AISERVICES_OPT_OUT_POLICY
+	BACKUP_POLICY
+	SERVICE_CONTROL_POLICY
+	TAG_POLICY
+	"""
+	aws_policy_type_list = ['SERVICE_CONTROL_POLICY', 'TAG_POLICY', 'BACKUP_POLICY', 'AISERVICES_OPT_OUT_POLICY']
+
+	# Find all the Organization Accounts
+	all_org_accounts = find_accounts_in_org()
+	if pPolicy:
+		print(f"Due to there being {len(all_org_accounts)} accounts in this Org, this process will likely take about {(len(all_org_accounts) / 2)} seconds")
+	else:
+		print(f"Due to there being {len(all_org_accounts)} accounts in this Org, this process will likely take about {5 + (len(all_org_accounts) / 10)} seconds")
+	# Specifying the root Org ID, get all the root OUs we'll have to traverse
+	root_OUs = get_root_OUS(root)
+	# Draw the Org itself and save it to the local filesystem
+	draw_org(root_OUs)
+
+	if pTiming:
+		print(f"{Fore.GREEN}\tThis script took {time() - begin_time:.2f} seconds{Fore.RESET}")
+		print()
+	print("Thank you for using this script")
 	print()
-print("Thank you for using this script")
-print()
