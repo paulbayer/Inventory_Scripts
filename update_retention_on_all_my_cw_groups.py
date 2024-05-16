81188b4e651311edb69563fcfd2f33892c4f3e72
paulbaye <paulbaye@amazon.com>
2024-05-16 10:00:52 -0400

    Updated to adhere to new python consistency

--- update_retention_on_all_my_cw_groups.py
+++ update_retention_on_all_my_cw_groups.py
@@ -1,137 +1,151 @@
 #!/usr/bin/env python3
 
-# import boto3
-import Inventory_Modules
-from Inventory_Modules import display_results
+import sys
+from os.path import split
+from Inventory_Modules import display_results, find_cw_groups_retention2, get_all_credentials
 from ArgumentsClass import CommonArguments
-from account_class import aws_acct_access
 from colorama import init, Fore
 from time import time
+from threading import Thread
+from queue import Queue
+from tqdm.auto import tqdm
 from botocore.exceptions import ClientError
 
 import logging
 
 init()
-__version__ = "2023.05.10"
-
-parser = CommonArguments()
-parser.multiprofile()
-parser.multiregion()
-parser.rootOnly()
-parser.rolestouse()
-parser.verbosity()
-parser.timing()
-parser.version(__version__)
-
-parser.my_parser.add_argument(
-	'+R', "--ReplaceRetention",
-	help="The retention you want to update to on all groups that match.",
-	default=None,
-	metavar="retention days",
-	type=int,
-	choices=[0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 2192, 2557, 2922, 3288, 3653],
-	dest="pRetentionDays")
-parser.my_parser.add_argument(
-	'-o', "--OldRetention",
-	help="The retention you want to change on all groups that match. Use '0' for 'Never'",
-	default=None,
-	metavar="retention days",
-	type=int,
-	choices=[0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 2192, 2557, 2922, 3288, 3653],
-	dest="pOldRetentionDays")
-args = parser.my_parser.parse_args()
-
-pProfiles = args.Profiles
-pRegionList = args.Regions
-pAccessRoles = args.AccessRoles
-pRetentionDays = args.pRetentionDays
-pOldRetentionDays = args.pOldRetentionDays
-pRootOnly = args.RootOnly
-pTiming = args.Time
-verbose = args.loglevel
-logging.basicConfig(level=args.loglevel, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
-
-##################
-
+__version__ = "2024.05.10"
 ERASE_LINE = '\x1b[2K'
-logging.info(f"Profiles: {pProfiles}")
-account_number_format = "12s"
-if pTiming:
-	begin_time = time()
+begin_time = time()
 
 
 ##################
+# Functions
+##################
+def parse_args(f_arguments):
+	script_path, script_name = split(sys.argv[0])
+	parser = CommonArguments()
+	parser.multiprofile()
+	parser.multiregion()
+	parser.extendedargs()
+	parser.rootOnly()
+	parser.rolestouse()
+	parser.save_to_file()
+	parser.verbosity()
+	parser.timing()
+	parser.version(__version__)
+	local = parser.my_parser.add_argument_group(script_name, 'Parameters specific to this script')
+
+	local.add_argument(
+		'+R', "--ReplaceRetention",
+		help="The retention you want to update to on all groups that match.",
+		default=None,
+		metavar="retention days",
+		type=int,
+		choices=[0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 2192, 2557, 2922, 3288, 3653],
+		dest="pRetentionDays")
+	local.add_argument(
+		'-o', "--OldRetention",
+		help="The retention you want to change on all groups that match. Use '0' for 'Never'",
+		default=None,
+		metavar="retention days",
+		type=int,
+		choices=[0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 2192, 2557, 2922, 3288, 3653],
+		dest="pOldRetentionDays")
+	return parser.my_parser.parse_args(f_arguments)
 
 
-def check_cw_groups_retention(faws_acct, fRegionList=None, faccess_roles:list=None):
-	ChildAccounts = faws_acct.ChildAccounts
+def check_cw_groups_retention(f_all_credential_list: list) -> list:
+	"""
+	This function will check the retention on all CW Groups in all accounts.
+	@param f_all_credential_list: Listing of all credentials for accounts we'll look into
+	@return: Returns a list of all CW Groups in all accounts provided by the credentials submitted.
+	"""
+
+	class FindCWGroups(Thread):
+		def __init__(self, queue):
+			Thread.__init__(self)
+			self.queue = queue
+
+		def run(self):
+			while True:
+				# Get the work from the queue and expand the tuple
+				c_account_credentials = self.queue.get()
+				pbar.update()
+				logging.info(f"De-queued info for account number {c_account_credentials['AccountId']}")
+				try:
+					CW_Groups = find_cw_groups_retention2(c_account_credentials)
+					if len(CW_Groups) > 0:
+						for logGroup in CW_Groups:
+							if 'retentionInDays' in logGroup.keys():
+								logGroup['Retention'] = logGroup['retentionInDays']
+							else:
+								logGroup['Retention'] = "Never"
+							logGroup['Name'] = logGroup['logGroupName']
+							logGroup['Size'] = logGroup['storedBytes']
+							logGroup['AccessKeyId'] = c_account_credentials['AccessKeyId']
+							logGroup['SecretAccessKey'] = c_account_credentials['SecretAccessKey']
+							logGroup['SessionToken'] = c_account_credentials['SessionToken']
+							logGroup['ParentProfile'] = c_account_credentials['Profile'] if c_account_credentials['Profile'] is not None else 'default'
+							logGroup['MgmtAccount'] = c_account_credentials['MgmtAccount']
+							logGroup['AccountId'] = c_account_credentials['AccountId']
+							logGroup['Region'] = c_account_credentials['Region']
+						AllCWLogGroups.extend(CW_Groups)
+
+				except KeyError as my_Error:
+					logging.error(f"Account Access failed - trying to access {c_account_credentials['AccountId']}")
+					logging.info(f"Actual Error: {my_Error}")
+					pass
+				except AttributeError as my_Error:
+					logging.error(f"Error: Likely that one of the supplied profiles was wrong")
+					logging.warning(my_Error)
+					continue
+				except ClientError as my_Error:
+					if 'AuthFailure' in str(my_Error):
+						logging.error(f"Authorization Failure accessing account {c_account_credentials['AccountId']} in {c_account_credentials['Region']} region")
+						logging.warning(f"It's possible that the region {c_account_credentials['Region']} hasn't been opted-into")
+						continue
+					else:
+						logging.error(f"Error: Likely throttling errors from too much activity")
+						logging.warning(my_Error)
+						continue
+				finally:
+					self.queue.task_done()
+
+	checkqueue = Queue()
+
 	AllCWLogGroups = []
-	account_credentials = {'Role': 'unset'}
-	if fRegionList is None:
-		fRegionList = ['us-east-1']
-	for account in ChildAccounts:
-		if account['MgmtAccount'] != account['AccountId'] and pRootOnly:
-			continue
-		logging.info(f"Connecting to account {account['AccountId']}")
+	WorkerThreads = min(len(f_all_credential_list), 25)
+	WorkerThreads = min(len(f_all_credential_list), 1)
+
+	pbar = tqdm(desc=f'Finding CloudWatch log groups from {len(f_all_credential_list)} accounts / regions',
+	            total=len(f_all_credential_list), unit=' locations'
+	            )
+
+	for x in range(WorkerThreads):
+		worker = FindCWGroups(checkqueue)
+		# Setting daemon to True will let the main thread exit even though the workers are blocking
+		worker.daemon = True
+		worker.start()
+
+	for credential in f_all_credential_list:
+		logging.info(f"Beginning to queue data - starting with {credential['AccountId']}")
 		try:
-			account_credentials = Inventory_Modules.get_child_access3(faws_acct, account['AccountId'], faws_acct.Region, faccess_roles)
-			if account_credentials['Success']:
-				logging.info(f"Connected to account {account['AccountId']} using role {account_credentials['Role']}")
-			else:
-				logging.info(f"Access to account {account['AccountId']} in region {faws_acct.Region} failed, after trying role {'' if len(account_credentials['RolesTried']) == 1 else 's'}{account_credentials['RolesTried']}")
-				continue
+			# While double parens are necessary below, if you're queuing multiple values, we're only queuing one right now.
+			# But if/ when we add fragment finding, I'm leaving this comment here to remind myself of that.
+			# checkqueue.put((credential))
+			checkqueue.put(credential)
 		except ClientError as my_Error:
 			if "AuthFailure" in str(my_Error):
-				logging.error(
-					f"{account['AccountId']}: Authorization failure using role: {account_credentials['Role']}")
-				logging.warning(my_Error)
-			elif str(my_Error).find("AccessDenied") > 0:
-				logging.error(
-					f"{account['AccountId']}: Access Denied failure using role: {account_credentials['Role']}")
-				logging.warning(my_Error)
-			else:
-				logging.error(
-					f"{account['AccountId']}: Other kind of failure using role: {account_credentials['Role']}")
-				logging.warning(my_Error)
-			continue
-		for region in fRegionList:
-			CW_Groups = dict()
-			try:
-				print(f"{ERASE_LINE}Checking account {account['AccountId']} in region {region}", end='\r')
-				# TODO: Will eventually support a filter for string fragments, and retention periods
-				CW_Groups = Inventory_Modules.find_cw_groups_retention2(account_credentials, region)
-				logging.info(
-					f"Root Account: {faws_acct.acct_number} Account: {account['AccountId']} Region: {region} | Found {len(CW_Groups['logGroups'])} groups")
-			except ClientError as my_Error:
-				if "AuthFailure" in str(my_Error):
-					logging.error(f"Authorization Failure accessing account {account['AccountId']} in {region} region")
-					logging.warning(f"It's possible that the region {region} hasn't been opted-into")
-					pass
-			if 'logGroups' in CW_Groups.keys():
-				for y in range(len(CW_Groups['logGroups'])):
-					if 'retentionInDays' in CW_Groups['logGroups'][y].keys():
-						CW_Groups['logGroups'][y]['Retention'] = Retention = CW_Groups['logGroups'][y]['retentionInDays']
-					else:
-						CW_Groups['logGroups'][y]['Retention'] = Retention = "Never"
-					CW_Groups['logGroups'][y]['Name'] = Name = CW_Groups['logGroups'][y]['logGroupName']
-					CW_Groups['logGroups'][y]['Size'] = Size = CW_Groups['logGroups'][y]['storedBytes']
-					CW_Groups['logGroups'][y]['AccessKeyId'] = account_credentials['AccessKeyId']
-					CW_Groups['logGroups'][y]['SecretAccessKey'] = account_credentials['SecretAccessKey']
-					CW_Groups['logGroups'][y]['SessionToken'] = account_credentials['SessionToken']
-					CW_Groups['logGroups'][y]['ParentProfile'] = faws_acct.credentials['Profile'] if faws_acct.credentials['Profile'] is not None else 'default'
-					CW_Groups['logGroups'][y]['MgmtAccount'] = faws_acct.MgmtAccount
-					CW_Groups['logGroups'][y]['AccountId'] = account_credentials['AccountId']
-					CW_Groups['logGroups'][y]['Region'] = region
-				# fmt = f'%-12s %-{account_number_format} %-15s %-10s %15d %-50s'
-				# print(fmt % (faws_acct.acct_number, account['AccountId'], region, Retention, Size, Name))
-				# print(f"{str(faws_acct.acct_number):{account_number_format}} {str(account['AccountId']):{account_number_format}} {region:15s} "
-				# 	  f"{str(Retention):10s} {'' if Retention == 'Never' else 'days'} {Size: >15,} {Name:50s}")
-				AllCWLogGroups.extend(CW_Groups['logGroups'])
-
-	return (AllCWLogGroups)
+				logging.error(f"Authorization Failure accessing account {credential['AccountId']} in {credential['Region']} region")
+				logging.warning(f"It's possible that the region {credential['Region']} hasn't been opted-into")
+				pass
+	checkqueue.join()
+	pbar.close()
+	return AllCWLogGroups
 
 
-def update_cw_groups_retention(fCWGroups=None, fOldRetentionDays=None, fRetentionDays=None):
+def update_cw_groups_retention(fCWGroups: list, fOldRetentionDays: int = None, fRetentionDays: int = None):
 	import boto3
 
 	if fOldRetentionDays is None:
@@ -154,7 +168,7 @@ def update_cw_groups_retention(fCWGroups=None, fOldRetentionDays=None, fRetentio
 				result = cw_client.put_retention_policy(
 					logGroupName=item['logGroupName'],
 					retentionInDays=fRetentionDays
-				)
+					)
 				print(f"Account: {item['AccountId']} in Region: {item['Region']} updated {item['logGroupName']} from {retentionPeriod} to {fRetentionDays} days")
 				Updated = True
 			else:
@@ -164,79 +178,82 @@ def update_cw_groups_retention(fCWGroups=None, fOldRetentionDays=None, fRetentio
 		except ClientError as my_Error:
 			logging.error(my_Error)
 			Success = False
-			return (Success)
-	return (Success)
+			return Success
+	return Success
 
 
 ##################
+# Main
+##################
 
+if __name__ == '__main__':
+	args = parse_args(sys.argv[1:])
 
-print()
-print(f"Checking for CW Log Groups... ")
-print()
+	pProfiles = args.Profiles
+	pRegionList = args.Regions
+	pAccounts = args.Accounts
+	pSkipAccounts = args.SkipAccounts
+	pSkipProfiles = args.SkipProfiles
+	pAccessRoles = args.AccessRoles
+	pRetentionDays = args.pRetentionDays
+	pOldRetentionDays = args.pOldRetentionDays
+	pRootOnly = args.RootOnly
+	pFilename = args.Filename
+	pTiming = args.Time
+	verbose = args.loglevel
+	logging.basicConfig(level=verbose, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+	logging.getLogger("boto3").setLevel(logging.CRITICAL)
+	logging.getLogger("botocore").setLevel(logging.CRITICAL)
+	logging.getLogger("s3transfer").setLevel(logging.CRITICAL)
+	logging.getLogger("urllib3").setLevel(logging.CRITICAL)
 
-print()
-display_dict = {'ParentProfile': {'DisplayOrder': 1, 'Heading': 'Parent Profile'},
-                'MgmtAccount'  : {'DisplayOrder': 2, 'Heading': 'Mgmt Acct'},
-                'AccountId'    : {'DisplayOrder': 3, 'Heading': 'Acct Number'},
-                'Region'       : {'DisplayOrder': 4, 'Heading': 'Region'},
-                'Retention'    : {'DisplayOrder': 5, 'Heading': 'Days Retention', 'Condition': ['Never']},
-                'Name'         : {'DisplayOrder': 7, 'Heading': 'CW Log Name'},
-                'Size'         : {'DisplayOrder': 6, 'Heading': 'Size (Bytes)'}}
+	print()
+	print(f"Checking for CW Log Groups... ")
+	print()
 
-# print(f"{str(faws_acct.acct_number):{account_number_format}} {str(account['AccountId']):{account_number_format}} {region:15s} "
-#       f"{str(Retention):10s} {'' if Retention == 'Never' else 'days'} {Size: >15,} {Name:50s}")
+	CredentialList = get_all_credentials(pProfiles, pTiming, pSkipProfiles, pSkipAccounts, pRootOnly, pAccounts, pRegionList, pAccessRoles)
+	SuccessfulAccountAccesses = [x for x in CredentialList if x['Success']]
+	AllChildAccounts = list(set([(x['MgmtAccount'], x['AccountId']) for x in SuccessfulAccountAccesses]))
+	RegionList = list(set([x['Region'] for x in SuccessfulAccountAccesses]))
 
-CWGroups = []
-AllChildAccounts = []
-RegionList = []
+	display_dict = {
+		# 'ParentProfile': {'DisplayOrder': 1, 'Heading': 'Parent Profile'},
+	                'MgmtAccount'  : {'DisplayOrder': 2, 'Heading': 'Mgmt Acct'},
+	                'AccountId'    : {'DisplayOrder': 3, 'Heading': 'Acct Number'},
+	                'Region'       : {'DisplayOrder': 4, 'Heading': 'Region'},
+	                'Retention'    : {'DisplayOrder': 5, 'Heading': 'Days Retention', 'Condition': ['Never']},
+	                'Name'         : {'DisplayOrder': 7, 'Heading': 'CW Log Name'},
+	                'Size'         : {'DisplayOrder': 6, 'Heading': 'Size (Bytes)'}}
 
-if pProfiles is None:  # Default use case from the classes
-	logging.info("Using whatever the default profile is")
-	aws_acct = aws_acct_access()
-	RegionList = Inventory_Modules.get_regions3(aws_acct, pRegionList)
-	logging.warning(f"Default profile will be used")
-	CWGroups.extend(check_cw_groups_retention(aws_acct, RegionList, pAccessRoles))
-# AllChildAccounts.extend(aws_acct.ChildAccounts)
-else:
-	logging.warning(f"These profiles are being checked {pProfiles}.")
-	ProfileList = Inventory_Modules.get_profiles(fprofiles=pProfiles, fSkipProfiles="skipplus")
-	logging.warning(ProfileList)
-	for profile in ProfileList:
-		aws_acct = aws_acct_access(profile)
-		logging.warning(f"Looking at {profile} account now... ")
-		RegionList = Inventory_Modules.get_regions3(aws_acct, pRegionList)
-		CWGroups.extend(check_cw_groups_retention(aws_acct, RegionList, pAccessRoles))
-	# AllChildAccounts.extend(aws_acct.ChildAccounts)
+	CWGroups = check_cw_groups_retention(CredentialList)
+	sorted_CWGroups = sorted(CWGroups, key=lambda k: (k['MgmtAccount'], k['AccountId'], k['Region'], k['Name']))
 
-AllChildAccounts = list(set([(x['MgmtAccount'], x['AccountId']) for x in CWGroups]))
+	display_results(sorted_CWGroups, display_dict, None, pFilename)
 
-display_results(CWGroups, display_dict, None)
-
-print(ERASE_LINE)
-totalspace = 0
-for i in CWGroups:
-	totalspace += i['storedBytes']
-print(f"Found {len(CWGroups)} log groups across {len(AllChildAccounts)} accounts across {len(RegionList)} regions, representing {totalspace / 1024 / 1024 / 1024:,.3f} GB")
-print(f"To give you a small idea - in us-east-1 - it costs $0.03 per GB per month to store (after 5GB).")
-if totalspace / 1024 / 1024 / 1024 <= 5.0:
-	print("Which means this is essentially free for you...")
-else:
-	print(f"This means you're paying about ${((totalspace / 1024 / 1024 / 1024) - 5) * 0.03:,.2f} per month in CW storage charges")
-
-if pRetentionDays is not None:
-	print(f"As per your request - updating ALL retention periods to {pRetentionDays} days")
-	print(f"")
-	UpdateAllRetention = input(f"This is definitely an intrusive command, so please confirm you want to do this (y/n): ") in ['Y', 'y']
-	if UpdateAllRetention:
-		print(f"Updating all log groups to have a {pRetentionDays} retention period")
-		update_cw_groups_retention(CWGroups, pOldRetentionDays, pRetentionDays)
-	else:
-		print(f"No changes made")
-print()
-if pTiming:
 	print(ERASE_LINE)
-	print(f"{Fore.GREEN}This script took {time() - begin_time:.2f} seconds{Fore.RESET}")
-print()
-print("Thank you for using this script")
-print()
+	totalspace = 0
+	for i in CWGroups:
+		totalspace += i['storedBytes']
+	print(f"Found {len(CWGroups)} log groups across {len(AllChildAccounts)} accounts across {len(RegionList)} regions, representing {totalspace / 1024 / 1024 / 1024:,.3f} GB")
+	print(f"To give you a small idea - in us-east-1 - it costs $0.03 per GB per month to store (after 5GB).")
+	if totalspace / 1024 / 1024 / 1024 <= 5.0:
+		print("Which means this is essentially free for you...")
+	else:
+		print(f"This means you're paying about ${((totalspace / 1024 / 1024 / 1024) - 5) * 0.03:,.2f} per month in CW storage charges")
+
+	if pRetentionDays is not None:
+		print(f"As per your request - updating ALL retention periods to {pRetentionDays} days")
+		print(f"")
+		UpdateAllRetention = input(f"This is definitely an intrusive command, so please confirm you want to do this (y/n): ") in ['Y', 'y']
+		if UpdateAllRetention:
+			print(f"Updating all log groups to have a {pRetentionDays} retention period")
+			update_cw_groups_retention(CWGroups, pOldRetentionDays, pRetentionDays)
+		else:
+			print(f"No changes made")
+	print()
+	if pTiming:
+		print(ERASE_LINE)
+		print(f"{Fore.GREEN}This script took {time() - begin_time:.2f} seconds{Fore.RESET}")
+	print()
+	print("Thank you for using this script")
+	print()
