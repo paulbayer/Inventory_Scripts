3864d8e0847f83507c33a20c29172367ce0f9a0b
paulbaye <paulbaye@amazon.com>
2024-05-18 01:53:21 -0400

    Added another script to the list of tools not to test with this script.

--- test_tools.sh
+++ test_tools.sh
@@ -21,7 +21,7 @@ function exists_in_list() {
 scripts_to_not_test="Inventory_Modules.py recovery_stack_ids.py lock_down_stack_sets_role.py ArgumentsClass.py \
 account_class.py ALZ_CheckAccount.py CT_CheckAccount.py delete_bucket_objects.py enable_drift_detection.py \
 find_my_LZ_versions.py move_stack_instances.py RunOnMultiAccounts.py UpdateRoleToMemberAccounts.py vpc_modules.py \
-recover_stack_ids.py setup.py decorators.py"
+recover_stack_ids.py setup.py decorators.py read_stackset_results.py"
 
 scripts_that_require_response="enable_drift_detection_stacksets.py"
 
